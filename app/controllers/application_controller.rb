d069133306c228b6ed83a3590aa7e8c28b9bd3c7
Shane Russell <shane1337@gmail.com>
2016-05-31 13:57:24 -0400

    Force SSL at the controller level, to avoid health-check ssl force (#69)
    * Rails 4 syntax for excluding the https redirect.
    
    * Force SSL at the controller level, to avoid health-check ssl force
    
    fixes #65

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  force_ssl if: :ssl_enabled?
   before_action :authenticate
   before_action :authorize
   before_action :configure_bgs
+  before_action :strict_transport_security
 
   def authenticate
     redirect_to "/auth/samlva" if current_user.nil?
@@ -22,6 +24,14 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   private
+
+  def ssl_enabled?
+    Rails.env.production? && !(request.path =~ /health-check/)
+  end
+
+  def strict_transport_security
+    response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains" if request.ssl?
+  end
 
   def configure_bgs
     BGSService.user = current_user
