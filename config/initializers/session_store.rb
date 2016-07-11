ac97e36bd6776ae942d41bae61ffdc7f33d244e4
Shane Russell <shane1337@gmail.com>
2016-07-11 16:52:31 -0400

    Sign CSS SSOi requests (#152)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,10 @@
 # Be sure to restart your server when you modify this file.
+options = {
+  key: '_caseflow_session',
+  secure: Rails.env.production?,
+  expire_after: 2.weeks
+}
 
-Rails.application.config.session_store :cookie_store, key: '_efolder-express_session', expire_after: 2.weeks, secure: Rails.env.production?
+options[:domain] = ENV["COOKIE_DOMAIN"] if ENV["COOKIE_DOMAIN"]
+
+Rails.application.config.session_store :cookie_store, options
