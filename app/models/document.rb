c626ab14b32ac31ea9ab7c32336483baa5ab4d28
Shane Russell <shane1337@gmail.com>
2016-07-29 09:40:13 -0400

    Default vbms_filename to empty string (#176)
    fixes #167

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,8 @@
 class Document < ActiveRecord::Base
   enum download_status: { pending: 0, success: 1, failed: 2 }
 
+  after_initialize { |document| document.vbms_filename ||= "" }
+
   def filename
     Zaru.sanitize! "#{vbms_filename.gsub(/\.\w+$/, '')}.#{preferred_extension}"
   end
