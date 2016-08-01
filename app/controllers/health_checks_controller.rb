dd7ccd611627328796eea7a5326833410d887513
Shane Russell <shane1337@gmail.com>
2016-08-01 15:35:34 -0400

    Factor out authoriztion logic and and test unauthorized admin.

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -1,6 +1,5 @@
 class HealthChecksController < ApplicationController
   skip_before_action :authenticate
-  skip_before_action :authorize
 
   def show
     render text: "Application server is healthy!"
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
   skip_before_action :authenticate, only: [:create]
-  skip_before_action :authorize
 
   def create
     session["user"] = User.from_css_auth_hash auth_hash
