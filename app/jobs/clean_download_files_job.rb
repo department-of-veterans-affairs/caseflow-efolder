fe5094efc531ec1c6f496560143f12eb9974ec52
Shane Russell <shane1337@gmail.com>
2016-08-02 12:13:47 -0400

    Cleanup all temporary files at midnight every night

---
+++ app/jobs/clean_download_files_job.rb
@@ -0,0 +1,17 @@
+require "fileutils"
+
+##
+# Cleans all files in the temporary download files directory
+#
+class CleanDownloadFilesJob < ActiveJob::Base
+  queue_as :default
+
+  def perform
+    Rails.logger.info "Cleaning all temporary files..."
+    FileUtils.rm_rf(Rails.application.config.download_filepath)
+  end
+
+  def max_attempts
+    1
+  end
+end
