1133e2b001f401cd0a40c4b5f45557620c5922ae
Shane Russell <shane1337@gmail.com>
2016-08-08 10:03:07 -0400

    Lock search button on form submission (#188)
    fixes #150

---
+++ app/assets/javascripts/loading_indicator.js
@@ -0,0 +1,18 @@
+// This is a candidate for caseflow commons
+window.LoadingIndicator = (function($) {
+  // private
+  function onSubmit(e) {
+    var $form = $(e.target).parents('.cf-form');
+    $form.addClass("cf-is-loading");
+  }
+
+  // public
+  return {
+    bind: function() {
+      $(".cf-form .cf-submit").on('click', onSubmit);
+
+      // remove loading flag for browsers with bfcaching
+      window.onpageshow = function() { $(".cf-form").removeClass("cf-is-loading"); };
+    }
+  };
+})($);
