c1d1afdcd2ad2e3844f088336c7d6c4f5281d9b0
Shane Russell <shane1337@gmail.com>
2016-08-24 08:21:38 -0400

    Add absolute path to sidekiq cron directory (#199)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: Rails.application.secrets.redis_url }
 
-  schedule_file = "config/sidekiq_cron.yml"
+  schedule_file = Rails.root + "config/sidekiq_cron.yml"
   if File.exists?(schedule_file) && Sidekiq.server?
     Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
   end
