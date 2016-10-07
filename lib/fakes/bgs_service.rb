4a99c03d44792c0ca149c2250c20d407105c1090
Alex Prokop <alex.prokop@gmail.com>
2016-10-07 12:29:40 -0400

    uses veteran info in fake bgsservice

--- lib/fakes/bgs_service.rb
+++ lib/fakes/bgs_service.rb
@@ -1,9 +1,9 @@
 class Fakes::BGSService
-  cattr_accessor :veteran_names
+  cattr_accessor :veteran_info
   cattr_accessor :sensitive_files
 
-  def self.fetch_veteran_name(file_number)
-    (veteran_names || {})[file_number]
+  def self.fetch_veteran_info(file_number)
+    (veteran_info || {})[file_number]
   end
 
   def self.check_sensitivity(file_number)
