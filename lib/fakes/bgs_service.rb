c7225891b5187dffa5e2c9a79db69eb246326b8d
Alex Prokop <alex.prokop@gmail.com>
2016-10-10 19:46:15 -0400

    tweaks formatting and naming

--- lib/fakes/bgs_service.rb
+++ lib/fakes/bgs_service.rb
@@ -4,7 +4,9 @@ class Fakes::BGSService
 
   def self.fetch_veteran_info(file_number)
     if file_number =~ /DEMO/
-      return { "first_name": "Test", "last_name": "User", "last_4_ssn": "1224" }
+      return { veteran_first_name: "Test",
+        veteran_last_name: "User",
+        veteran_last_four_ssn: "1224" }
     end
     (veteran_info || {})[file_number]
   end
