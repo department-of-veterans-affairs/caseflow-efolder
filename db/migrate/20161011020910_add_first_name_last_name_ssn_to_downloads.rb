e2055c22a9bb11273278caa9337afa48dc398d38
Alex Prokop <alex.prokop@gmail.com>
2016-10-11 12:19:39 -0400

    uses find_each instead of all, db migation works still

--- db/migrate/20161011020910_add_first_name_last_name_ssn_to_downloads.rb
+++ db/migrate/20161011020910_add_first_name_last_name_ssn_to_downloads.rb
@@ -6,9 +6,16 @@ class AddFirstNameLastNameSsnToDownloads < ActiveRecord::Migration
     add_column :downloads, :veteran_first_name, :string
     add_column :downloads, :veteran_last_four_ssn, :string
 
-    Download.all.each do |download|
-      first, last = download.veteran_name.split(' ', 2)
-      download.update(veteran_first_name: first, veteran_last_name: last)
+    Download.find_each do |download|
+      if download.veteran_name
+        # This isn't going to be exactly right in all cases--
+        # if one's first name were "Carly Rae" and last name
+        # "Jepsen", for instance, we would split on the wrong
+        # space. However, historical Download rows are kept
+        # for record-keeping, so it's probably acceptable.
+        first, last = download.veteran_name.split(' ', 2)
+        download.update(veteran_first_name: first, veteran_last_name: last)
+      end
     end
 
     remove_column :downloads, :veteran_name
@@ -16,7 +23,7 @@ class AddFirstNameLastNameSsnToDownloads < ActiveRecord::Migration
   def down
     add_column :downloads, :veteran_name, :string
 
-    Download.all.each do |download|
+    Download.find_each do |download|
       full_name = "#{download.veteran_first_name} #{download.veteran_last_name}"
       download.update(veteran_name: full_name)
     end
