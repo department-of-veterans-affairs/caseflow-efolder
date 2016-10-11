034e0a08dfa77cb0b2b2d6ca0c270ad7e7ba45c6
Alex Prokop <alex.prokop@gmail.com>
2016-10-11 15:21:36 -0400

    add demo method to bgs fake

--- lib/fakes/bgs_service.rb
+++ lib/fakes/bgs_service.rb
@@ -6,14 +6,16 @@ class Fakes::BGSService
     file_number =~ /DEMO/
   end
 
+  def self.demo_veteran_info
+    {
+      "veteran_first_name" => "Test",
+      "veteran_last_name" => "User",
+      "veteran_last_four_ssn" => "1224"
+    }
+  end
+
   def self.fetch_veteran_info(file_number)
-    if demo?(file_number)
-      return {
-        "veteran_first_name" => "Test",
-        "veteran_last_name" => "User",
-        "veteran_last_four_ssn" => "1224"
-      }
-    end
+    return demo_veteran_info if demo?(file_number)
     (veteran_info || {})[file_number]
   end
 
