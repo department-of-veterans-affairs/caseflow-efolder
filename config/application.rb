75e16bb2072a949803320614d66cacb9a1c7c167
JD Pagano <jonathanpagano@gmail.com>
2016-10-18 17:37:24 -0400

    Move lib/utils to app/utils

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require all utilities
-Dir[File.expand_path('lib/utils/*.rb')].each { |file| require file }
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
