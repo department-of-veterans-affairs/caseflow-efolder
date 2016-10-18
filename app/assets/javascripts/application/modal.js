c75788cae09c51049d585adb33b67969f6602621
Alex Prokop <alex.prokop@gmail.com>
2016-10-18 15:26:28 -0400

    modal is focusable

--- app/assets/javascripts/application/modal.js
+++ app/assets/javascripts/application/modal.js
@@ -1,17 +1,39 @@
 //= require jquery
 
+/**
+ * Accessible, reusable modal component.
+ *
+ * Requires certain class names to function properly:
+ * - "cf-modal": the top level modal element
+ * - "cf-modal-body": the modal body
+ * - "cf-action-openmodal" Placed outside the modal. Triggers modal display.
+ * - "cf-action-closemodal": Placed inside the modal. Closes the modal.
+ * - "cf-modal-startfocus": the element that receives focus when the modal is opened.
+ *      Also used to trap focus inside the modal for a11y.
+ * - "cf-modal-endfocus": the last focusable element in the modal
+ *
+ * Accessibility:
+ * - When the modal is closed, returns keyboard focus to the element
+ *   that triggered the modal display
+ * - Naively traps keyboard if the class "cf-modal-startfocus" is
+ *   placed on the first focusable element and "cf-modal-endfocus"
+ *   is placed on the last focusable element.
+ *   TODO(alex): check out a more robust solution if this proves to
+ *   be unwieldy, e.g. https://github.com/davidtheclark/focus-trap
+ *
+ */
 window.Modal = (function($) {
 
-  // set by openModal. focus will be returned to this element
-  // after the modal is closed.
-  // important for a11y, so keyboard users have a sensible flow.
+  // Focus will be returned to this element
+  // after the modal is closed. Important for a11y,
+  // so keyboard users have a sensible flow.
   var returnEl;
 
   function openModal(e) {
     e.preventDefault();
     var target = $(e.target).attr("href");
     $(target).addClass("active");
-    $('.cf-modal-title').focus();
+    $('.cf-modal-startfocus').focus();
     returnEl = e.target;
   }
 
@@ -30,9 +52,19 @@ window.Modal = (function($) {
 
   function onKeyDown(e) {
     var escKey = (e.which === 27);
+    var tabKey = (e.which === 9);
 
     if (escKey) {
       $('.cf-modal').trigger('click');
+    }
+
+
+    if (tabKey) {
+      debugger;
+      if ($('.cf-modal-endfocus').is(':focus')) {
+        e.preventDefault();
+        $('.cf-modal-startfocus').focus();
+      }
     }
   }
 
