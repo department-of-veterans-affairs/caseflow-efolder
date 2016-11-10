c2aec1794f22465e82c587cd1dae318c60e3a6be
Ori Schwartz <orischwartz@users.noreply.github.com>
2016-11-10 17:15:04 -0500

    only log sentry user if there's a current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_raven_user
-    if ENV["SENTRY_DSN"]
+    if current_user && ENV["SENTRY_DSN"]
       # Raven sends error info to Sentry.
       Raven.user_context(
         id: current_user.id,
