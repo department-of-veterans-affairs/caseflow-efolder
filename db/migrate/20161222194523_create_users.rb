77065fdd9922828d2652101b76040b62f0ba9d32
Shane Russell <shane.russell.va@gmail.com>
2017-01-02 09:30:52 -0500

    Fix the user migration

--- db/migrate/20161222194523_create_users.rb
+++ db/migrate/20161222194523_create_users.rb
@@ -1,5 +1,5 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+  def up
     create_table :users do |t|
       t.string :css_id, null: false
       t.string :station_id, null: false
@@ -12,11 +12,18 @@ class CreateUsers < ActiveRecord::Migration
     add_belongs_to :downloads, :user, index: true, foreign_key: true
 
     (Search.all + Download.all).each do |record|
-      next unless record.css_id
+      next unless (record.css_id && record.user_station_id)
 
       user = User.find_or_create_by(css_id: record.css_id, station_id: record.user_station_id)
-      user.update(email: record.email) if record.email
+
+      email = record.is_a?(Download) ? record.searches.last.email : record.email
+      user.update(email: email) if email
+
       record.update(user: user)
     end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
