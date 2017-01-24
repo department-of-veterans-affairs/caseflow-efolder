da282600084898f0d9bb1bd81c7506298a81ffa8
Alex Prokop <alex.prokop@gmail.com>
2017-01-24 14:07:38 -0500

    adds dev values for /metrics

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,8 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # URL for feedback app. If not present, link points to Sharepoint.
-  # This can also be in Jenkins.
   ENV["CASEFLOW_FEEDBACK_URL"] = "https://dsva-appeals-feedback-demo-1748368704.us-gov-west-1.elb.amazonaws.com/"
+
+  ENV["METRICS_USERNAME"] = "caseflow"
+  ENV["METRICS_PASSWORD"] = "caseflow"
 end
