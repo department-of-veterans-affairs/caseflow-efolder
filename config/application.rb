7f3000057ced2110deea1e96991684181db357ab
Nemo Djuric <nemo.djuric@gmail.com>
2017-02-16 15:53:15 -0500

    Added redis gem and setup configuration files for it.

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,8 @@ module CaseflowEfolder
 
     config.active_job.queue_adapter = :sidekiq
 
+    config.cache_store = :redis_store, Rails.application.secrets.redis_url_cache, { expires_in: 24.hours }
+
     # default to no analytics (production only)
     config.analytics_account = nil
   end
