a0e402925195e88d5e23713eb4f4a38b7ffa3aa8
Alex Prokop <alex.prokop@gmail.com>
2017-03-01 17:20:44 -0500

    only keeps 2 polling requests open at once

--- app/assets/javascripts/application/download_status.js
+++ app/assets/javascripts/application/download_status.js
@@ -1,22 +1,31 @@
 window.DownloadStatus = (function($) {
-  var id, currentStatus;
+  var id, currentStatus, openRequests = 0;
 
   // public
   return {
     intervalID: null,
     recheck: function() {
       if (id) {
+        openRequests++;
         $.getJSON("/downloads/" + id).then(function(download) {
+          openRequests-- ;
           if (download.status != currentStatus) { location.reload(); }
+        }, function(){
+          openRequests--;
         });
       }
     },
 
     init: function(downloadId, downloadStatus) {
+      var recheck = this.recheck;
       id = downloadId;
       currentStatus = downloadStatus;
 
-      this.intervalID = window.setInterval(this.recheck, 1000);
+      this.intervalID = window.setInterval(function() {
+        if (openRequests <= 2) {
+          recheck();
+        }
+      }, 1000);
     }
   };
 })($);
