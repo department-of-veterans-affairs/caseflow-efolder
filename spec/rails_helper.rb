eb36d7226ee8a81ee26b346e742eeb289e4ad089
Nemo Djuric <nemo.djuric@gmail.com>
2017-03-03 12:18:15 -0500

    Added method check and landing page. Added spec test.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,6 +79,10 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = false
 
+  config.after(:each) do
+    Rails.cache.clear
+  end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
