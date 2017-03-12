e43392864585deda07f2610320389e77405ed00f
Sjones352 <sjones@tistatech.com>
2017-03-11 20:08:39 -0500

    add the Back screenshot

---
+++ app/assets/javascripts/application/scroll.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+ 
+ //Check to see if the window is top if not then display button
+  $(window).scroll(function(){
+   if ($(this).scrollTop() > 50) {
+      $('.scrollToTop').fadeIn(200);
+   } else {
+      $('.scrollToTop').fadeOut(200);
+    }
+  });
+  
+  //Click event to scroll to top
+  $('.scrollToTop').click(function(){
+    $('html, body').animate({scrollTop : 0},100);
+   return false;
+  });
+  
+});
