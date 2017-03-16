bec2db21b76c66c531bf895250c30c61459a1496
Sjones352 <sjones@tistatech.com>
2017-03-15 21:11:37 -0400

    changes

--- app/assets/javascripts/application/scroll.js
+++ app/assets/javascripts/application/scroll.js
@@ -2,14 +2,14 @@ $(window).ready(function(){
  //Check to see if the window is top if not then display button
   $(window).scroll(function(){
    if ($(this).scrollTop() > 50) {
-      $('.scroll-to-top').fadeIn(200);
+      $('.scroll-bar').fadeIn(200);
    } else {
-      $('.scroll-to-top').fadeOut(200);
+      $('.scroll-bar').fadeOut(200);
     }
   });
   
   //Click event to scroll to top
-  $('.scroll-to-top').click(function(){
+  $('.scroll-bar').click(function(){
     $('html, body').animate({scrollTop : 0},100);
    return false;
    });
