538300b78fdd1248ee7e38193b926ae5ee5a2b21
aroltsch <anya.roltsch@gmail.com>
2017-03-31 09:00:00 -0400

    Migrate doc_type to type_id

--- db/migrate/20170327122224_migrate_doc_type_to_type_id.rb
+++ db/migrate/20170327122224_migrate_doc_type_to_type_id.rb
@@ -1,7 +1,11 @@
 class MigrateDocTypeToTypeId < ActiveRecord::Migration
   def change
-    Document.find_each do |d|
-      d.update(type_id: d.doc_type) unless d.type_id
+    say_with_time("Migrating doc_type to type_id in documents table") do
+      pbar = ProgressBar.new(total: Document.find(:all).size)
+      Document.find_each do |d|
+        pbar.increment
+        d.update(type_id: d.doc_type) unless d.type_id
+      end
     end
   end
 end
