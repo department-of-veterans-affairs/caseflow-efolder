0e0231247c820f5c1f677f936e5f57cda1f3d714
aroltsch <anya.roltsch@gmail.com>
2017-03-31 09:39:44 -0400

    Add progress bar

--- db/migrate/20170327122224_migrate_doc_type_to_type_id.rb
+++ db/migrate/20170327122224_migrate_doc_type_to_type_id.rb
@@ -1,7 +1,7 @@
 class MigrateDocTypeToTypeId < ActiveRecord::Migration
   def change
     say_with_time("Migrating doc_type to type_id in documents table") do
-      pbar = ProgressBar.new(total: Document.find(:all).size)
+      pbar = ProgressBar.create(total: Document.all.size)
       Document.find_each do |d|
         pbar.increment
         d.update(type_id: d.doc_type) unless d.type_id
