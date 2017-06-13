7d2b732aa5582c61c34b74ec4bcc860dcf3e3724
aroltsch <anya.roltsch@gmail.com>
2017-06-13 15:05:48 -0400

    Implement FE changes for VVA

--- app/jobs/demo_get_download_files_job.rb
+++ app/jobs/demo_get_download_files_job.rb
@@ -1,9 +1,10 @@
-class FakeVBMSService
+class FakeService
   cattr_accessor :errors, :max_time
 
   def self.fetch_document_file(_document)
-    sleep(rand(FakeVBMSService.max_time))
-    fail VBMS::ClientError if FakeVBMSService.errors && rand(5) == 3
+    sleep(rand(FakeService.max_time))
+    fail VBMS::ClientError if FakeService.errors && rand(5) == 3
+    fail VVA::ClientError if FakeService.errors && rand(5) == 2
     "this is some document, woah!"
   end
 end
@@ -14,10 +15,10 @@ class DemoGetDownloadFilesJob < ActiveJob::Base
   def perform(download)
     demo = DemoGetDownloadManifestJob::DEMOS[download.file_number] || DemoGetDownloadManifestJob::DEMOS["DEMODEFAULT"]
 
-    FakeVBMSService.errors = demo[:error]
-    FakeVBMSService.max_time = demo[:max_file_load]
+    FakeService.errors = demo[:error]
+    FakeService.max_time = demo[:max_file_load]
 
-    download_documents = DownloadDocuments.new(download: download, vbms_service: FakeVBMSService)
+    download_documents = DownloadDocuments.new(download: download, vbms_service: FakeService, vva_service: FakeService)
     download_documents.download_and_package
   end
 end
