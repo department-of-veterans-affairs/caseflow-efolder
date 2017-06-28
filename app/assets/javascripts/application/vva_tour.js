7bd35789c2f3dfea1168b3255aa1b7b995f1d50c
Marian Lumba <mmlumba@gmail.com>
2017-06-28 16:15:05 -0400

    multiline functionality yeah

--- app/assets/javascripts/application/vva_tour.js
+++ app/assets/javascripts/application/vva_tour.js
@@ -1,6 +1,6 @@
 // Define the tour!
 var tour = {
-  id: "hello-hopscotch",
+  id: "vva-tour",
   steps: [
     {
       title: "Welcome to eFolder Express",
@@ -9,7 +9,6 @@ var tour = {
       placement: "right"
     },
     {
-      title: "Retrieve documents",
       content: "Type in a Veteran ID number to find a veteran's eFolder.",
       target: "file_number",
       placement: "bottom"
@@ -18,11 +17,33 @@ var tour = {
       title: "Press search",
       content: "Click on the search button to retrieve the veteran's eFolder.",
       target: "submit-ee-search",
-      placement: "left"
+      placement: "left",
+      multipage: true
+    },
+    {
+      content: "The total documents found from the veteran's eFolder and the Legacy Content Manager can be found here. The source of each document is listed in the table below.",
+      target: "retrieve_documents",
+      placement: "bottom",
+      xOffset: "center",
+      multipage: true,
+      showPrevButton: false
+    },
+    {
+      content: "The total documents retrieved are listed in the completed tab. Document errors during the retrieval process are displayed in the errors tab along with their source.",
+      target: "vva_notice",
+      placement: "bottom",
+      showPrevButton: false
+    },
+    {
+      content: "Once all the files are successfully retrieved, click the 'Download' eFolder button at the top or bottom of the page to download the complete eFolder.",
+      target: "download_efolder_button",
+      placement: "right",
+      showPrevButton: false
     }
-  ],
-  showPrevButton: true
+  ]
 };
+
+//TODO(marian): fix why the 5th coachmark doesn't show up after the files have been retrieved
 
 // Start the tour!
 window.VVATour = (function($){
