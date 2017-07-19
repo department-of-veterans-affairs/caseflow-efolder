b76f7f72bb5f6dca28c8176a2babc7eae8e2e0e1
Mark Benjamin <mark@navahq.com>
2017-07-19 17:46:54 -0400

    Updating api to return vbms_document_id

--- spec/requests/api/v1/file_spec.rb
+++ spec/requests/api/v1/file_spec.rb
@@ -115,17 +115,20 @@ describe "File API v1", type: :request do
                 {
                   id: download.documents[2].id,
                   type_id: "825",
-                  received_at: "2015-09-06T01:00:00.000Z"
+                  received_at: "2015-09-06T01:00:00.000Z",
+                  vbms_document_id: document.document_id
                 },
                 {
                   id: download.documents[1].id,
                   type_id: "123",
-                  received_at: "2017-02-01T00:00:00.000Z"
+                  received_at: "2017-02-01T00:00:00.000Z",
+                  vbms_document_id: "1"
                 },
                 {
                   id: download.documents[0].id,
                   type_id: "124",
-                  received_at: "2017-04-03T00:00:00.000Z"
+                  received_at: "2017-04-03T00:00:00.000Z",
+                  vbms_document_id: "2"
                 }
               ]
             }
