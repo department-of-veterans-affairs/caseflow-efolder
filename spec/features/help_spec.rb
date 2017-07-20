3413659ef5f0f6c603e7b048c84a4213157c18e8
Mark Benjamin <mark@navahq.com>
2017-07-20 15:58:30 -0400

    Revert "Catching JS error"
    This reverts commit 39685a6436dbbc004a32972e60eee575f7f0687a.

--- spec/features/help_spec.rb
+++ spec/features/help_spec.rb
@@ -3,15 +3,7 @@ RSpec.feature "Help" do
     visit("/")
     click_on "first last"
     expect(page).to have_content("Help")
-
-    # rubocop:disable Lint/HandleExceptions
-    begin
-      click_on "Help"
-    rescue Capybara::Poltergeist::JavascriptError
-      # Embedding YouTube Links can cause JavaScript Errors on browsers that don't
-      # support HTML5 video. Since it's unrelated to our code, ignore this JS error.
-    end
-    # rubocop:enable Lint/HandleExceptions
+    click_on "Help"
     expect(page).to have_content("Frequently Asked Questions")
   end
 end
