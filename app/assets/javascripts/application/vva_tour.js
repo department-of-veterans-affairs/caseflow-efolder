7f9be434f12fc1f226de0eab492a863c1cf66ce7
Nick Heiner <nth23@cornell.edu>
2017-07-20 18:35:28 -0400

    refactoring, but not everything works yet

--- app/assets/javascripts/application/vva_tour.js
+++ app/assets/javascripts/application/vva_tour.js
@@ -1,75 +1,99 @@
-window.VVATour = (function($){
-  var calloutMgr = hopscotch.getCalloutManager();
+window.VVATour = (function($) {
+  var calloutManager = hopscotch.getCalloutManager();
 
-  var vvaCallout1 = {
-    id: 'vva-tour-1',
-    target: 'vva-tour-1',
-    placement: 'bottom',
-    content: 'Downloads from eFolder Express now include Virtual VA documents.'
-  };
+  function initCallouts(callouts, onAllClosed) {
+    var countClosed = 0;
 
-  var vvaCallout2 = {
-    id: 'vva-tour-2',
-    content: 'The total number of documents that will be retrieved from each database is listed here.',
-    target: 'vva-tour-2',
-    placement: 'top'
-  };
+    function ensureCalloutExists(callout) {
+      return calloutManager.getCallout(callout) || calloutManager.createCallout(callout);
+    }
 
-  var vvaCallout3 = {
-    id: 'vva-tour-3',
-    content: 'The Source column shows the name of the database from which the file will be retrieved.',
-    target: 'vva-tour-3',
-    placement: 'left'
-  };
+    callouts.forEach(function(callout) {
+      callout.onClose = function() {
+        countClosed++;
+        if (countClosed === callouts.length) {
+          onAllClosed();
+        }
+      };
+      calloutManager.createCallout(callout);
+    });
+  }
 
-  var vvaCallout4 = {
-    id: 'vva-tour-4',
-    target: 'vva-tour-4',
-    placement: 'bottom',
-    xOffset: 'center',
-    content: 'The total number of documents that will be downloaded from each database is listed here.'
-  };
+  function setCurrentPageCallouts(callouts) {
+    $(function() {
+
+      var hideTutorialText = 'Hide tutorial';
+      var $hideTutorialLink = $('<a href="#" id="cf-view-coachmarks-link">' + hideTutorialText + '</a>');
+      $('#hide-tutorial-parent').prepend($hideTutorialLink)
+
+      var allCalloutsClosed;
+
+      function onAllCalloutsClosed() {
+        $hideTutorialLink.text('Show tutorial');
+        allCalloutsClosed = true;
+      }
+
+      function createCallouts() {
+        allCalloutsClosed = false;
+        initCallouts(callouts, onAllCalloutsClosed);
+      }
+
+      $('#cf-view-coachmarks-link').click(function() {
+        if (allCalloutsClosed) {
+          $hideTutorialLink.text(hideTutorialText);
+          createCallouts();
+        } else {
+          calloutManager.removeAllCallouts();
+          onAllCalloutsClosed();
+        }
+      });
+
+      createCallouts();
+    });
+  }
+
+  function initNewPage() {
+    setCurrentPageCallouts([{
+      id: 'vva-tour-1',
+      target: 'vva-tour-1',
+      placement: 'bottom',
+      content: 'Downloads from eFolder Express now include Virtual VA documents.'
+    }]);
+  }
+
+  function initConfirmPage() {
+    var vvaCallout2 = {
+      id: 'vva-tour-2',
+      content: 'The total number of documents that will be retrieved from each database is listed here.',
+      target: 'vva-tour-2',
+      placement: 'top'
+    };
+
+    var vvaCallout3 = {
+      id: 'vva-tour-3',
+      content: 'The Source column shows the name of the database from which the file will be retrieved.',
+      target: 'vva-tour-3',
+      placement: 'left'
+    };
+  }
+
+  function initProgressPage() {
+    var vvaCallout4 = {
+      id: 'vva-tour-4',
+      target: 'vva-tour-4',
+      placement: 'bottom',
+      xOffset: 'center',
+      content: 'The total number of documents that will be downloaded from each database is listed here.'
+    };
+  }
 
   return {
+    initNewPage: initNewPage,
+    initConfirmPage: initConfirmPage,
+    initProgressPage: initProgressPage,
+
     closeExistingCallouts: function() {
-      calloutMgr.removeAllCallouts();
+      calloutManager.removeAllCallouts();
     },
-    bind: function() {
-      var callouts = [];
-      function verifyElementExists(coachmarkID) {
-        return document.getElementById(coachmarkID);
-      }
-
-      function ensureCalloutExists(calloutId) {
-        return calloutMgr.getCallout(calloutId) || calloutMgr.createCallout(calloutId);
-      }
-
-      if (verifyElementExists('vva-tour-1')) {
-        ensureCalloutExists(vvaCallout1);
-        callouts = [vvaCallout1];
-      }
-      // The confirming downloads page has 2 coachmarks, hence they're in the same conditional
-      else if (verifyElementExists('vva-tour-2')){
-        ensureCalloutExists(vvaCallout2);
-        ensureCalloutExists(vvaCallout3);
-        callouts = [vvaCallout2, vvaCallout3];
-      }
-      else if (verifyElementExists('vva-tour-4')){
-        ensureCalloutExists(vvaCallout4);
-        callouts = [vvaCallout4];
-      }
-      $('#cf-view-coachmarks-link').on('click', function() {
-        callouts.forEach(function(callout) {
-          if (calloutMgr.getCallout(callout.id)) {
-            calloutMgr.removeCallout(callout.id);
-            $('#cf-view-coachmarks-link').text('Show tutorial');
-          }
-          else {
-            ensureCalloutExists(callout);
-            $('#cf-view-coachmarks-link').text('Hide tutorial');
-          }
-        })
-      })
-    }
-  }
+  };
 })($);
