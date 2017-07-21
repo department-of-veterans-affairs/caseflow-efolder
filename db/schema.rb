a9fff5ae43d668847dd4b84a63fcb53e06410d95
Nick Heiner <nth23@cornell.edu>
2017-07-21 17:06:14 -0400

    REALLY RESET SCHEMA THIS TIME

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721203804) do
+ActiveRecord::Schema.define(version: 20170707025718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
