cdbc635dcd8c5438ac28bfc5d45a7ca1f508f72c
Mark Benjamin <mark@navahq.com>
2017-08-04 14:40:24 -0400

    Fix stale document error

--- app/models/fetcher.rb
+++ app/models/fetcher.rb
@@ -3,7 +3,8 @@ class Fetcher
   attr_accessor :document, :external_service
 
   def content
-    document.update_attributes!(started_at: Time.zone.now)
+    document.lock!.update_attributes!(started_at: Time.zone.now)
+
     result = S3Service.fetch_content(document.s3_filename) || cache_in_s3
     document.update_attributes!(
       completed_at: Time.zone.now,
