38d4e2113f9a0d1bc54fdc40f79b2e6f2c1d99c6
Mark Benjamin <mark@navahq.com>
2017-08-07 11:13:14 -0400

    Changing to two private methods

--- app/models/fetcher.rb
+++ app/models/fetcher.rb
@@ -4,7 +4,11 @@ class Fetcher
 
   def content(save_document_metadata: true)
     return cached_content if cached_content
-    cache_in_s3(save_document_metadata: save_document_metadata)
+    if save_document_metadata
+      download_from_service_and_record
+    else
+      download_from_service
+    end
   end
 
   private
@@ -13,17 +17,20 @@ class Fetcher
     @cached_content ||= S3Service.fetch_content(document.s3_filename)
   end
 
-  def cache_in_s3(save_document_metadata: true)
-    document.update_attributes!(started_at: Time.zone.now) if save_document_metadata
+  def download_from_service
+    external_service.fetch_document_file(document).tap do |result|
+      S3Service.store_file(document.s3_filename, result)
+    end
+  end
 
-    result = external_service.fetch_document_file(document)
-    S3Service.store_file(document.s3_filename, result)
-
-    document.update_attributes!(
-      completed_at: Time.zone.now,
-      download_status: :success,
-      size: result.try(:bytesize)
-    ) if save_document_metadata
-    result
+  def download_from_service_and_record
+    document.update_attributes!(started_at: Time.zone.now)
+    download_from_service.tap do |result|
+      document.update_attributes!(
+        completed_at: Time.zone.now,
+        download_status: :success,
+        size: result.try(:bytesize)
+      )
+    end
   end
 end
