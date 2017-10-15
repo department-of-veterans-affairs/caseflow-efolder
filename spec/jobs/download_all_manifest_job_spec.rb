824e622fae5207128e7da3445897772fbb077162
Sunil Sadasivan <sunil@navahq.com>
2017-10-15 18:25:29 -0400

    Create a download_all_manifest add add more expected behavior

--- spec/jobs/download_all_manifest_job_spec.rb
+++ spec/jobs/download_all_manifest_job_spec.rb
@@ -1,11 +1,11 @@
-describe DownloadManifestJob do
+describe DownloadAllManifestJob do
   context "#perform" do
     let(:download) { Download.create }
 
     context "when document list is empty" do
       before do
         allow(VBMSService).to receive(:fetch_documents_for).and_return([])
-        DownloadVBMSManifestJob.perform_now(download)
+        DownloadAllManifestJob.perform_now(download)
       end
 
       it "saves download status as :no_documents" do
@@ -31,7 +31,7 @@ describe DownloadManifestJob do
             OpenStruct.new(document_id: "2")
           ])
 
-        DownloadVBMSManifestJob.perform_now(download)
+        DownloadAllManifestJob.perform_now(download)
       end
 
       it "saves download status as pending confirmation and creates documents" do
@@ -39,5 +39,34 @@ describe DownloadManifestJob do
         expect(download.documents.count).to eq(2)
       end
     end
+
+    context "when VVA client fails" do
+      before do
+        allow(VVAService).to receive(:fetch_documents_for).and_raise(VVA::ClientError)
+      end
+      it "saves download status as :vbms_connection_error" do
+        expect(DownloadAllManifestJob.perform_now(download)).to be_nil
+        expect(download.reload).to be_vva_connection_error
+      end
+    end
+
+    context "when VBMS returns non-empty document list and VVA returns an error" do
+      before do
+        allow(VBMSService).to receive(:fetch_documents_for).and_return(
+          [
+            OpenStruct.new(document_id: "1"),
+            OpenStruct.new(document_id: "2")
+          ])
+        allow(VVAService).to receive(:fetch_documents_for).and_raise(VVA::ClientError)
+
+        DownloadAllManifestJob.perform_now(download)
+      end
+
+      it "saves download status as vva_connection_error and does not create documents" do
+        expect(DownloadAllManifestJob.perform_now(download)).to be_nil
+        expect(download.reload).to be_vva_connection_error
+      end
+    end
+
   end
 end
