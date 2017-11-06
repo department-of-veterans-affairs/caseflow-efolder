93392c6476b0c9f1647acb79c032b87ffd03ded2
Nick Heiner <nth23@cornell.edu>
2017-11-06 13:58:45 -0500

    lint

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -1,7 +1,7 @@
 class HealthChecksController < ApplicationController
   skip_before_action :authenticate
   newrelic_ignore_apdex
-  
+
   def show
     render json: { healthy: true }.merge(Rails.application.config.build_version || {})
   end
