bbb50e0814c689645d1013a333271fd4e37bc405
aroltsch <anya.roltsch@gmail.com>
2017-11-14 14:41:43 -0500

    Make a correction

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -1,7 +1,7 @@
 class Manifest < ActiveRecord::Base
   has_many :manifest_statuses
   has_many :user_manifests
-  has_many :documents
+  has_many :records
 
   validates :file_number, presence: true, uniqueness: true
 end
