e7da00c1dfd5b9b1e41d4ba5b8fcb4fadeeaecad
Nick Heiner <nth23@cornell.edu>
2017-11-16 17:16:21 -0500

    rescue EOFError

--- app/jobs/download_files_job.rb
+++ app/jobs/download_files_job.rb
@@ -1,6 +1,14 @@
 class DownloadFilesJob < ActiveJob::Base
   queue_as :default
 
+  # Sometimes, a VA backend will randomly fail. When we retry, it generally works.
+  # We will explicitly rescue and retry here, because otherwise the first
+  # failure will be logged to Sentry. For this error, we only want the last one
+  # to be logged to Sentry.
+  rescue_from EOFError do
+    retry_job
+  end
+
   def perform(download)
     download_documents = DownloadDocuments.new(download: download)
 
