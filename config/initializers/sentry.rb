32e456b4e6d4b6c3f317d95adfb6f0b902173930
Nick Heiner <nth23@cornell.edu>
2017-11-16 17:35:03 -0500

    update comment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,9 +3,7 @@ if ENV['SENTRY_DSN']
     config.dsn = ENV['SENTRY_DSN']
 
     # Sometimes, a VA backend will randomly fail. When we retry, it generally works.
-    # We will explicitly rescue and retry here, because otherwise the first
-    # failure will be logged to Sentry. For this error, we only want the last one
-    # to be logged to Sentry.
+    # We do not need to log this to Sentry, because it's not actionable for us.
     config.excluded_exceptions += ['EOFError']
   end
 end
