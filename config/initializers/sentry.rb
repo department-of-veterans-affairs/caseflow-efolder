3be8dbbaaae9a56da8d869bbe8fcefebfb6a628a
Nick Heiner <nth23@cornell.edu>
2017-11-16 17:33:17 -0500

    try a different approach

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,11 @@
 if ENV['SENTRY_DSN']
   Raven.configure do |config|
     config.dsn = ENV['SENTRY_DSN']
+
+    # Sometimes, a VA backend will randomly fail. When we retry, it generally works.
+    # We will explicitly rescue and retry here, because otherwise the first
+    # failure will be logged to Sentry. For this error, we only want the last one
+    # to be logged to Sentry.
+    config.excluded_exceptions += ['EOFError']
   end
 end
