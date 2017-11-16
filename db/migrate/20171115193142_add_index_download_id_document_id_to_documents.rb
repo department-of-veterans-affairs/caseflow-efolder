71bc22f76fdee36ccd0833db72e8317ce29d8b0c
Anya Roltsch <aroltsch@users.noreply.github.com>
2017-11-16 16:28:40 -0500

    Modify migration to add index on documents using concurrently (#714)

--- db/migrate/20171115193142_add_index_download_id_document_id_to_documents.rb
+++ db/migrate/20171115193142_add_index_download_id_document_id_to_documents.rb
@@ -1,6 +1,8 @@
 class AddIndexDownloadIdDocumentIdToDocuments < ActiveRecord::Migration
+  disable_ddl_transaction!
+
   def change
-    add_index :documents, [:download_id, :document_id], using: :btree
+    add_index :documents, [:download_id, :document_id], algorithm: :concurrently, using: :btree
     remove_index :documents, [:download_id]
   end
 end
