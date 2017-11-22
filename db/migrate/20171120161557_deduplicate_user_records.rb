3699f69abe994a8d30a785f47135b4ef6137cdc0
lowellnava <lowell@navahq.com>
2017-11-22 13:01:37 -0500

    Set css_id to newest case-insensitive match instead of all uppercase

--- db/migrate/20171120161557_deduplicate_user_records.rb
+++ db/migrate/20171120161557_deduplicate_user_records.rb
@@ -1,11 +1,6 @@
 class DeduplicateUserRecords < ActiveRecord::Migration
   # rubocop:disable Metrics/MethodLength
   def up
-    # Run these statements as part of a single transaction for safety and thoroughness (we could potentially add
-    # a new user after the delete operation took place and capitalizing each letter would then cause a primary
-    # key conflict). Also, this transaction will probably be very speedy so any locking effects will be small.
-    execute "begin;"
-
     # Update css_id field in downloads table to reference older version of duplicate user.
     execute <<-SQL
       with t as (
@@ -42,6 +37,23 @@ class DeduplicateUserRecords < ActiveRecord::Migration
       where user_id = t.duplicate_id;
     SQL
 
+    # Replace css_ids for older user records with the newest user record's css_id casing.
+    execute <<-SQL
+      update users t
+      set css_id = c.css_id
+      from users a
+      join (
+        select
+          max(id) max_id, upper(css_id) upper_css_id
+        from users
+        group by upper(css_id)
+      ) b
+      on upper(a.css_id) = b.upper_css_id
+      join users c
+      on c.id = b.max_id
+      where a.css_id = t.css_id;
+    SQL
+
     # Delete newer duplicate user records.
     execute <<-SQL
       delete
@@ -56,14 +68,6 @@ class DeduplicateUserRecords < ActiveRecord::Migration
           and a.created_at < b.created_at
       );
     SQL
-
-    # Capitalize all css_ids in the user table.
-    execute <<-SQL
-      update users set css_id = upper(css_id);
-    SQL
-
-    # Commit the transaction.
-    execute "commit;"
   end
   # rubocop:enable Metrics/MethodLength
 
