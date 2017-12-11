17612f02422523ec40b2b29c8caa614bce760aba
aroltsch <anya.roltsch@gmail.com>
2017-12-11 10:32:36 -0500

    Implement v2 save files in s3 job

--- app/jobs/v2/save_files_in_s3_job.rb
+++ app/jobs/v2/save_files_in_s3_job.rb
@@ -1,10 +1,8 @@
-class SaveFilesInS3Job < ActiveJob::Base
+class V2::SaveFilesInS3Job < ActiveJob::Base
   queue_as :default
 
   def perform(manifest_source)
-    manifest.source.records.each do |record|
-      record.fetch!
-    end
+    manifest_source.records.each(&:fetch!)
   end
 
   def max_attempts
