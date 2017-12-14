7d30305c64a4f93c47c53b422524252f41565455
aroltsch <anya.roltsch@gmail.com>
2017-12-14 16:22:59 -0500

    Sentry - Ignore exceptions when VBMS/VVA are in maintenance

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,5 +5,9 @@ if ENV['SENTRY_DSN']
     # Sometimes, a VA backend will randomly fail. When we retry, it generally works.
     # We do not need to log this to Sentry, because it's not actionable for us.
     config.excluded_exceptions += ['EOFError']
+
+    # Do not send Sentry alerts when external systems are in maintenance mode
+    # to test it: simulate these errors
+    config.excluded_exceptions += [/Could not get JDBC Connection/, /Maintenance - VBMS/, /upstream connect error or disconnect/]
   end
 end
