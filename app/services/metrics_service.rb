ed516edd285c06a71f2c9606fe317cf28e9a2240
Nick Heiner <nth23@cornell.edu>
2017-12-14 14:53:20 -0500

    update metrics service

--- app/services/metrics_service.rb
+++ app/services/metrics_service.rb
@@ -1,11 +1,21 @@
 require "benchmark"
+require "dogapi"
 
 # see https://dropwizard.github.io/metrics/3.1.0/getting-started/ for abstractions on metric types
 class MetricsService
+  datadog_api_key = ENV["DATADOG_API_KEY"]
+  if datadog_api_key.nil?
+    Rails.logger.warn "Env var DATADOG_API_KEY is not set, so DataDog metrics will not be tracked."
+    # Setting the API key to an empty string will make tracking requests silently fail, which is what we want.
+    datadog_api_key = ""
+  end
+
+  @dog = Dogapi::Client.new(datadog_api_key)
+  @app = "eFolder"
+
   # rubocop:disable Metrics/MethodLength
   def self.record(description, service: nil, name: "unknown")
     return_value = nil
-    app = "eFolder"
 
     Rails.logger.info("STARTED #{description}")
     stopwatch = Benchmark.measure do
@@ -15,7 +25,9 @@ class MetricsService
     if service
       metric = PrometheusService.send("#{service}_request_latency".to_sym)
 
-      metric.set({ app: app, name: name }, stopwatch.real)
+      latency = stopwatch.real
+      metric.set({ app: @app, name: name }, latency)
+      emit_datadog_point("request_latency", latency, service, name)
 
     end
 
@@ -24,16 +36,29 @@ class MetricsService
   rescue
     if service
       metric = PrometheusService.send("#{service}_request_error_counter".to_sym)
-      metric.increment(app: app, name: name)
+      metric.increment(app: @app, name: name)
+      emit_datadog_point("request_error", 1, service, name)
     end
 
-    # Reraise the same error. We don't want to interfere at all in normal error handling
-    # this is just to capture the metric
+    # Re-raise the same error. We don't want to interfere at all in normal error handling.
+    # This is just to capture the metric
     raise
   ensure
     if service
       metric = PrometheusService.send("#{service}_request_attempt_counter".to_sym)
-      metric.increment(app: app, name: name)
+      metric.increment(app: @app, name: name)
+      emit_datadog_point("request_attempt", 1, service, name)
     end
+  end
+
+  private_class_method def self.emit_datadog_point(metric_name, metric_value, service, endpoint_name)
+    @dog.emit_point("caseflow.service.#{metric_name}", metric_value,
+                    host: `hostname`.strip, type: "counter",
+                    tags: [
+                      "app:#{@app}",
+                      "service:#{service}",
+                      "env:#{Rails.env}",
+                      "endpoint_name:#{endpoint_name}"
+                    ])
   end
 end
