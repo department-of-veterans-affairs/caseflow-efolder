bdd535ea67d9ca5b37c8f3dfde404a6fac29d3b8
Sunil Sadasivan <31223104+sunil-sadasivan@users.noreply.github.com>
2017-12-18 10:49:41 -0500

    Revert "Update metrics service to send to DataDog (#776)" (#788)
    This reverts commit e404b44b1828f36fa23eb0571207fb7b925c3a02.

--- app/services/metrics_service.rb
+++ app/services/metrics_service.rb
@@ -1,12 +1,11 @@
 require "benchmark"
-require "dogapi"
 
 # see https://dropwizard.github.io/metrics/3.1.0/getting-started/ for abstractions on metric types
 class MetricsService
   # rubocop:disable Metrics/MethodLength
-  @app = "eFolder"
   def self.record(description, service: nil, name: "unknown")
     return_value = nil
+    app = "eFolder"
 
     Rails.logger.info("STARTED #{description}")
     stopwatch = Benchmark.measure do
@@ -16,18 +15,8 @@ class MetricsService
     if service
       metric = PrometheusService.send("#{service}_request_latency".to_sym)
 
-      latency = stopwatch.real
-      metric.set({ app: @app, name: name }, latency)
-      DataDogService.emit_gauge(
-        metric_group: "service",
-        metric_name: "request_latency",
-        metric_value: latency,
-        app_name: @app,
-        attrs: {
-          service: service,
-          endpoint: name
-        }
-      )
+      metric.set({ app: app, name: name }, stopwatch.real)
+
     end
 
     Rails.logger.info("FINISHED #{description}: #{stopwatch}")
@@ -35,30 +24,16 @@ class MetricsService
   rescue
     if service
       metric = PrometheusService.send("#{service}_request_error_counter".to_sym)
-      metric.increment(app: @app, name: name)
-      increment_datadog_counter("request_error", service, name)
+      metric.increment(app: app, name: name)
     end
 
-    # Re-raise the same error. We don't want to interfere at all in normal error handling.
-    # This is just to capture the metric.
+    # Reraise the same error. We don't want to interfere at all in normal error handling
+    # this is just to capture the metric
     raise
   ensure
     if service
       metric = PrometheusService.send("#{service}_request_attempt_counter".to_sym)
-      metric.increment(app: @app, name: name)
-      increment_datadog_counter("request_attempt", service, name)
+      metric.increment(app: app, name: name)
     end
-  end
-
-  private_class_method def self.increment_datadog_counter(metric_name, service, endpoint_name)
-    DataDogService.increment_counter(
-      metric_group: "service",
-      metric_name: metric_name,
-      app_name: @app,
-      attrs: {
-        service: service,
-        endpoint: endpoint_name
-      }
-    )
   end
 end
