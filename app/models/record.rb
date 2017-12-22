ba57757c7bcf1acb634c07042ddf0b4411f7d675
Anya Roltsch <aroltsch@users.noreply.github.com>
2017-12-22 06:57:09 -0500

    V2 api - use VBMS ListDocuments (#799)
    * V2 api - use VBMS ListDocuments
    
    * Ignore maintenance exceptions in API calls

--- app/models/record.rb
+++ app/models/record.rb
@@ -51,10 +51,10 @@ class Record < ActiveRecord::Base
     find_or_initialize_by(manifest_source: manifest_source, external_document_id: document.document_id).tap do |t|
       t.assign_attributes(
         type_id: document.type_id,
-        type_description: document.type_description,
+        type_description: document.try(:type_description),
         mime_type: document.mime_type,
         received_at: document.received_at,
-        jro: document.jro,
+        jro: document.try(:jro),
         source: document.source
       )
       t.save!
