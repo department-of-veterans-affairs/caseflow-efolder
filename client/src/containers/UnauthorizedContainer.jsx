dcc05b64f5fad12418c076958653599485abe210
lowellnava <lowell@navahq.com>
2018-01-26 10:46:27 -0500

    Add unauthorized alert to react app

---
+++ client/src/containers/UnauthorizedContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import AppSegment from '@department-of-veterans-affairs/appeals-frontend-toolkit/components/AppSegment';
+
+export default class UnauthorizedContainer extends React.PureComponent {
+  render() {
+    return <main className="usa-grid">
+      <AppSegment extraClassNames="cf-app-msg-screen" filledBackground>
+        <h1 class="cf-msg-screen-heading">Drat!</h1>
+        <h2 class="cf-msg-screen-deck">You aren't authorized to use eFolder Express yet.</h2>
+        <p class="cf-msg-screen-text">
+          <Link to="/help">Click here</Link> to find out how you can request access to eFolder Express.
+        </p>
+      </AppSegment>
+    </main>;
+  }
+}
