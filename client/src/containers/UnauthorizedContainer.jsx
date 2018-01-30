a5ef6428a89cc08816b722063349e1e749923f6b
lowellnava <lowell@navahq.com>
2018-01-30 10:17:15 -0500

    Use StatusMessage in UnauthorizedContainer

--- client/src/containers/UnauthorizedContainer.jsx
+++ client/src/containers/UnauthorizedContainer.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import AppSegment from '@department-of-veterans-affairs/appeals-frontend-toolkit/components/AppSegment';
+import StatusMessage from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/StatusMessage';
 
 export default class UnauthorizedContainer extends React.PureComponent {
   render() {
     return <main className="usa-grid">
-      <AppSegment extraClassNames="cf-app-msg-screen" filledBackground>
-        <h1 className="cf-msg-screen-heading">Drat!</h1>
-        <h2 className="cf-msg-screen-deck">You aren't authorized to use eFolder Express yet.</h2>
-        <p className="cf-msg-screen-text">
-          <Link to="/help">Click here</Link> to find out how you can request access to eFolder Express.
-        </p>
-      </AppSegment>
+      <StatusMessage title="Drat!">
+        You aren't authorized to use eFolder Express yet.
+        <br />
+        <Link to="/help">Click here</Link> to find out how you can request access to eFolder Express.
+      </StatusMessage>
     </main>;
   }
 }
