aef97734a88ba07b51a938c32b385dae3db42be1
lowellnava <lowell@navahq.com>
2018-02-27 19:52:42 -0500

    Display alert if document source unavailable

--- client/src/containers/DownloadListContainer.jsx
+++ client/src/containers/DownloadListContainer.jsx
@@ -3,10 +3,13 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import AppSegment from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/AppSegment';
+import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
 
+import AlertBanner from '../components/AlertBanner';
 import { startDocumentDownload } from '../apiActions';
 import ManifestDocumentsTable from '../components/ManifestDocumentsTable';
 import DownloadPageFooter from '../components/DownloadPageFooter';
+import { MANIFEST_SOURCE_FETCH_STATE } from '../Constants';
 import { aliasForSource } from '../Utils';
 
 const startDownloadButtonLabel = 'Start retrieving efolder';
@@ -15,14 +18,32 @@ class DownloadListContainer extends React.PureComponent {
   startDownload = () => this.props.startDocumentDownload(this.props.manifestId, this.props.csrfToken);
 
   render() {
-    const totalDocumentsCount = this.props.documentSources.reduce((cnt, src) => cnt + src.number_of_documents, 0);
-    const documentCountNote = this.props.documentSources.map((src) => (
-      `${src.number_of_documents} from ${aliasForSource(src.source)}`)).join(' and ');
+    let totalDocumentsCount = 0;
+    const documentCountDescriptions = [];
+    const unavailableDocumentSources = [];
+
+    for (const src of this.props.documentSources) {
+      totalDocumentsCount += src.number_of_documents;
+      documentCountDescriptions.push(`${src.number_of_documents} from ${aliasForSource(src.source)}`);
+      if (src.status === MANIFEST_SOURCE_FETCH_STATE.FAILED) {
+        unavailableDocumentSources.push(src.source);
+      }
+    }
+
+    const unavailableSourceText = unavailableDocumentSources.join(', ');
 
     return <React.Fragment>
       <AppSegment filledBackground>
-        <p>eFolder Express found a total of {totalDocumentsCount} documents ({documentCountNote}) for&nbsp;
-          {this.props.veteranName} #{this.props.veteranId}. Verify the Veteran ID and click the&nbsp;
+        { unavailableSourceText &&
+          <AlertBanner title={`Can't connect to ${unavailableSourceText}`} alertType="warning">
+            <p>Please give {unavailableSourceText} a few moments to come back online,
+              then try searching for the eFolder again.</p>
+            <p><Link to="/">Try again</Link></p>
+          </AlertBanner>
+        }
+
+        <p>eFolder Express found a total of {totalDocumentsCount} documents ({documentCountDescriptions.join(' and ')})
+          for {this.props.veteranName} #{this.props.veteranId}. Verify the Veteran ID and click the&nbsp;
           {startDownloadButtonLabel} button below to start retrieving the eFolder.
         </p>
         <p>
