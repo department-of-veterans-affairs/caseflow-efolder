e885c58f9bc45932da3215e13f78e7252a58f707
lowellrex <lowell@navahq.com>
2018-03-15 10:39:21 -0400

    Change append operator to push

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 Rails.application.config.assets.precompile += %w( react-app.js stats.js )
 Rails.application.config.assets.precompile += %w( favicon.ico )
-Rails.application.config.assets.precompile += %w( *.woff *.woff2 *.eot *.ttf )
+Rails.application.config.assets.precompile << %w( *.woff *.woff2 *.eot *.ttf )
