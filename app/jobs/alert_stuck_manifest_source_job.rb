980f64916378effbc33e02713bdae2c2b61a710f
lowellrex <lowell@navahq.com>
2018-03-20 11:49:06 -0400

    Add alert to remove this job at the end of May

--- app/jobs/alert_stuck_manifest_source_job.rb
+++ app/jobs/alert_stuck_manifest_source_job.rb
@@ -2,6 +2,11 @@ class AlertStuckManifestSourceJob < ApplicationJob
   queue_as :default
 
   def perform
+    if Date.current >= Date.new(2018, 5, 31)
+      # Remove the associate entry in config/sidekiq_cron.yml as well.
+      ExceptionLogger.capture("Consider removing the AlertStuckManifestSourceJob if we haven't seen a stuck ManifestSource in a while")
+    end
+
     stuck_manifest_ids = ManifestSource.find_by_sql("select * from manifest_sources where status = 1 and created_at + interval '1 day' < current_timestamp").map(&:id)
 
     unless stuck_manifest_ids.empty?
