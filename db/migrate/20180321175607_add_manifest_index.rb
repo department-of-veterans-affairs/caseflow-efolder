e5f1878304ea011fc3e42047dc5737bee0e560b2
Mark Benjamin <mark@navahq.com>
2018-03-21 15:27:42 -0400

    Splitting out migration

--- db/migrate/20180321175607_add_manifest_index.rb
+++ db/migrate/20180321175607_add_manifest_index.rb
@@ -1,23 +1,7 @@
 class AddManifestIndex < ActiveRecord::Migration[5.1]
-  safety_assured
+  disable_ddl_transaction!
 
-  def up
-    manifest_exists_for_file_number = {}
-    manifests_to_remove = []
-
-    Manifest.find_each do |manifest|
-      if manifest_exists_for_file_number[manifest[:file_number]]
-        manifests_to_remove.push(manifest.id)
-      end
-      manifest_exists_for_file_number[manifest[:file_number]] = true
-    end
-
-    Manifest.where(id: manifests_to_remove).destroy_all
-
-    add_index :manifests, :file_number, unique: true
-  end
-
-  def down
-    remove_index :manifests, :file_number
+  def change
+    add_index :manifests, :file_number, unique: true, algorithm: :concurrently
   end
 end
