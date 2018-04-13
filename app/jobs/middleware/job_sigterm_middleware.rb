81a32e25d5fd0f6bf9efda4a106bd57d3a3bfb28
Mark Benjamin <mdbenjam@gmail.com>
2018-04-13 17:52:46 -0400

    Adding middleware to reset visibility timeout on sigterm and reducing lock time

---
+++ app/jobs/middleware/job_sigterm_middleware.rb
@@ -0,0 +1,16 @@
+class JobSigtermMiddleware
+  # When Shoryuken receives a call to shutdown (i.e. SIGTERM), it stops jobs that
+  # are not complete. Since some of our jobs have long visibility timeouts they
+  # may not be restarted for a long time. This gets around that problem by
+  # forcing the visibility timeout on a job that did not complete to be 0, as is
+  # suggested here: https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-visibility-timeout.html#terminating-message-visibility-timeout
+  def call(_worker, _queue, msg, _body)
+    job_finished = false
+    yield
+    # Note we only get to this point if the job was allowed to finish.
+    job_finished = true
+  ensure
+    # Ensures always execute even on SIGTERMs (unless we cannot wrap up fast enough and are killed)
+    msg.visibility_timeout = 0 if !job_finished
+  end
+end
