8697943e7b1aedee363c05a1f66df5f437fce3bf
Lowell Wood <32683958+lowellrex@users.noreply.github.com>
2018-04-24 15:02:06 -0400

    Automatically increase lease on SQS message while running job (#999)
    * Automatically increase lease on SQS message while running job
    * Remove job to release SQS messages back to queue when we receive SIGTERM

--- config/initializers/shoryuken.rb
+++ config/initializers/shoryuken.rb
@@ -1,11 +1,11 @@
 require "#{Rails.root}/app/jobs/middleware/job_prometheus_metric_middleware"
 require "#{Rails.root}/app/jobs/middleware/job_data_dog_metric_middleware"
 require "#{Rails.root}/app/jobs/middleware/job_raven_reporter_middleware"
-require "#{Rails.root}/app/jobs/middleware/job_sigterm_middleware"
 
 # set up default exponential backoff parameters
 ActiveJob::QueueAdapters::ShoryukenAdapter::JobWrapper
-  .shoryuken_options(retry_intervals: [3.seconds, 30.seconds, 5.minutes, 30.minutes, 2.hours, 5.hours])
+  .shoryuken_options(auto_visibility_timeout: true,
+                     retry_intervals: [3.seconds, 30.seconds, 5.minutes, 30.minutes, 2.hours, 5.hours])
 
 if Rails.application.config.sqs_endpoint
   # override the sqs_endpoint
@@ -14,13 +14,12 @@ end
 
 if Rails.application.config.sqs_create_queues
   # create the development queues
-  Shoryuken::Client.sqs.create_queue({ queue_name: ActiveJob::Base.queue_name_prefix + '_low_priority' })
-  Shoryuken::Client.sqs.create_queue({ queue_name: ActiveJob::Base.queue_name_prefix + '_med_priority' })
-  Shoryuken::Client.sqs.create_queue({ queue_name: ActiveJob::Base.queue_name_prefix + '_high_priority' })
+  Shoryuken::Client.sqs.create_queue(queue_name: ActiveJob::Base.queue_name_prefix + "_low_priority")
+  Shoryuken::Client.sqs.create_queue(queue_name: ActiveJob::Base.queue_name_prefix + "_med_priority")
+  Shoryuken::Client.sqs.create_queue(queue_name: ActiveJob::Base.queue_name_prefix + "_high_priority")
 end
 
 Shoryuken.configure_server do |config|
-
   Rails.logger = Shoryuken::Logging.logger
   Rails.logger.level = Logger::INFO
 
@@ -29,6 +28,5 @@ Shoryuken.configure_server do |config|
     chain.add JobPrometheusMetricMiddleware
     chain.add JobDataDogMetricMiddleware
     chain.add JobRavenReporterMiddleware
-    chain.add JobSigtermMiddleware
   end
 end
