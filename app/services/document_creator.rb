aa10043cf7033edf08dde9a9fa09c9b47b68d967
Mark Benjamin <mdbenjam@gmail.com>
2018-05-29 14:03:07 +0000

    In race condition rescue ActiveRecord::RecordNotUnique (#1012)
    Catching ActiveRecord::RecordNotUnique that can be thrown when two jobs try to create records at the same time. The error just means another thread has beaten this one and the records are already created.

--- app/services/document_creator.rb
+++ app/services/document_creator.rb
@@ -42,6 +42,15 @@ class DocumentCreator
         Record.create_from_external_document(manifest_source, document)
       end
     end
+  rescue ActiveRecord::RecordNotUnique
+    # This can happen when two jobs are run simultaneously. Both jobs delete the records
+    # then the first job adds all the records and finishes its transaction. The records are
+    # committed so when the second job then tries to add the same documents, it encounters
+    # the unique constraint on the table and an error is raised.
+    #
+    # We can ignore this exception because the race condition that causes it
+    # means that another thread just created these records.
+    Rails.logger.info "ActiveRecord::RecordNotUnique thrown for ManifestSource #{manifest_source.id}"
   end
 
   # Override the getter to return only non-restricted documents
