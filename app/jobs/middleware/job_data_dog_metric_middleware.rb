f2f4bfd423016b8807a24b67d71f7cd53640f5ce
Nemo Djuric <nemo.djuric@gmail.com>
2018-06-18 17:13:00 -0400

    Rescue when sending metrics to Prometheus and DataDog

--- app/jobs/middleware/job_data_dog_metric_middleware.rb
+++ app/jobs/middleware/job_data_dog_metric_middleware.rb
@@ -1,19 +1,25 @@
 class JobDataDogMetricMiddleware
-  def call(_worker, _queue, _msg, body)
+  def call(_worker, queue, _msg, body)
     job_class = body["job_class"]
 
     stopwatch = Benchmark.measure do
       yield
     end
 
-    DataDogService.emit_gauge(
-      metric_group: "job",
-      metric_name: "elapsed_time",
-      metric_value: stopwatch.real,
-      app_name: "eFolder",
-      attrs: {
-        job: job_class
-      }
-    )
+    begin
+      DataDogService.emit_gauge(
+        metric_group: "job",
+        metric_name: "elapsed_time",
+        metric_value: stopwatch.real,
+        app_name: "eFolder",
+        attrs: {
+          job: job_class
+        }
+      )
+    rescue StandardError => ex
+      tags = { job: job_class, queue: queue }
+      context = { message: body }
+      Raven.capture_exception(ex, tags: tags, extra: context)
+    end
   end
 end
--- app/jobs/middleware/job_prometheus_metric_middleware.rb
+++ app/jobs/middleware/job_prometheus_metric_middleware.rb
@@ -1,18 +1,25 @@
 class JobPrometheusMetricMiddleware
-  def call(_worker, _queue, _msg, body)
-    job_class = body["job_class"]
+  def call(_worker, queue, _msg, body)
+    @job_class = body["job_class"]
 
     yield
   rescue StandardError
-    PrometheusService.background_jobs_error_counter.increment(name: job_class)
+    PrometheusService.background_jobs_error_counter.increment(name: @job_class)
 
     # reraise the same error. This lets Shoryuken's retry logic kick off
     # as normal, but we still capture the error
     raise
   ensure
-    PrometheusService.background_jobs_attempt_counter.increment(name: job_class)
+    record_and_push_metrics(queue, body)
+  end
 
+  def record_and_push_metrics(queue, body)
+    PrometheusService.background_jobs_attempt_counter.increment(name: @job_class)
     # No need to actually push in test/development/demo
     PrometheusService.push_metrics! if Rails.env.production?
+  rescue StandardError => ex
+    tags = { job: job_class, queue: queue }
+    context = { message: body }
+    Raven.capture_exception(ex, tags: tags, extra: context)
   end
 end
