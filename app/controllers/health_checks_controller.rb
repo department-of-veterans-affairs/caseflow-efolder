a5790eec1431a593b67104001572220524de5c3e
Robert A Alexander (USDS/OMB/EOP//VA) <Robert.Alexander@va.gov>
2018-07-06 10:27:12 -0400

    check first pass

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -3,7 +3,24 @@ class HealthChecksController < ApplicationController
   skip_before_action :check_v2_app_access
   newrelic_ignore_apdex
 
+  def is_healthy?
+    # Check health of sidecar services
+    if not Rails.deploy_env?(:prod)
+      Caseflow::PushgatewayService.is_healthy?
+    else
+      true
+    end
+  end
+
   def show
-    render json: { healthy: true }.merge(Rails.application.config.build_version || {})
+    self.is_healthy?
+
+    # TODO: wire check into controller
+    healthy = true
+
+    body = {
+      healthy: healthy
+    }.merge(Rails.application.config.build_version || {})
+    render(json: body, status: healthy ? :ok : :service_unavailable)
   end
 end
