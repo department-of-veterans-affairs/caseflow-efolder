a074df12deab733ad0f0292d05b249fb5aaed0eb
Peter Karman <peter@truss.works>
2019-04-29 15:02:13 -0500

    Add ResultSetTooBig

--- app/exceptions/vbms_error.rb
+++ app/exceptions/vbms_error.rb
@@ -4,8 +4,12 @@
 class VBMSError < DependencyError
   class Transient < VBMSError; end
   class DocumentNotFound < VBMSError; end
+  class ResultSetTooBig < VBMSError; end
 
   KNOWN_ERRORS = {
+    # https://sentry.ds.va.gov/department-of-veterans-affairs/efolder/issues/4812/events/314288/
+    /Requested result set exceeds acceptable size/ => "VBMSError::ResultSetTooBig",
+
     /Document not found/ => "VBMSError::DocumentNotFound",
 
     # Example: https://sentry.ds.va.gov/department-of-veterans-affairs/efolder/issues/2161/
