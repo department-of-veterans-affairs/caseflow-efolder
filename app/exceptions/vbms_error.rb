6288a1f50481d4a904628078e7892bcd8cd181e3
Peter Karman <pkarman@users.noreply.github.com>
2019-05-01 08:05:05 -0500

    add VBMSError::FilenumberDoesNotExist (#1087)

--- app/exceptions/vbms_error.rb
+++ app/exceptions/vbms_error.rb
@@ -5,12 +5,16 @@ class VBMSError < DependencyError
   class Transient < VBMSError; end
   class DocumentNotFound < VBMSError; end
   class ResultSetTooBig < VBMSError; end
+  class FilenumberDoesNotExist < VBMSError; end
 
   KNOWN_ERRORS = {
     # https://sentry.ds.va.gov/department-of-veterans-affairs/efolder/issues/4812/events/314288/
     /Requested result set exceeds acceptable size/ => "VBMSError::ResultSetTooBig",
 
     /Document not found/ => "VBMSError::DocumentNotFound",
+
+    # https://sentry.ds.va.gov/department-of-veterans-affairs/efolder/issues/4812/events/315479/
+    /File Number does not exist within the system/ => "VBMSError::FilenumberDoesNotExist",
 
     # Example: https://sentry.ds.va.gov/department-of-veterans-affairs/efolder/issues/2161/
     /HTTPClient::ReceiveTimeoutError: execution expired/ => "VBMSError::Transient",
