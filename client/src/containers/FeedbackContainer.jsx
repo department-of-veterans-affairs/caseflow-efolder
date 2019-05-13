305262f73d54dc28095815738aa2d4dc8acbff19
Nemo Djuric <nemo.djuric@gmail.com>
2019-05-12 23:20:03 -0400

    Eslint

--- client/src/containers/FeedbackContainer.jsx
+++ client/src/containers/FeedbackContainer.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
-import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
 import StatusMessage from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/StatusMessage';
 
 export default class FeedbackContainer extends React.PureComponent {
   render() {
     return <StatusMessage title="Having a technical difficulty?">
-      Submit a ticket to the Caseflow team using <a href="https://yourIT.va.gov" target="_blank">YourIT</a>. The YourIT link is also available on most VA issued workstations. To better assist, please ensure you provide the URL or web address associated to the issue in the ticket.<br /><br />
+      Submit a ticket to the Caseflow team using <a href="https://yourIT.va.gov" target="_blank"
+        rel="noopener noreferrer">YourIT</a>. The YourIT link is also available on most VA issued workstations.
+       To better assist, please ensure you provide the URL or web address associated to the issue in the ticket.
+      <br /><br />
 
-The Caseflow Technical Support Team does not issue or manage access to Caseflow. Please do not submit a ticket through YourIT to the Caseflow team if you are looking for access to Caseflow or another VA product.<br /><br />
+The Caseflow Technical Support Team does not issue or manage access to Caseflow. Please do not submit a ticket
+ through YourIT to the Caseflow team if you are looking for access to Caseflow or another VA product.
+      <br /><br />
 
-Access is approved and granted via email through your management team and your local CSEM Information Security Officer (ISO). Please email your management team for access guidance.
-      </StatusMessage>;
+Access is approved and granted via email through your management team and your local
+CSEM Information Security Officer (ISO). Please email your management team for access guidance.
+    </StatusMessage>;
   }
 }
