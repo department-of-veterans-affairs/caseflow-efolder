05ef37c76574358aff774fa13b503fe5aa9cee5e
Nemo Djuric <nemo.djuric@gmail.com>
2019-05-12 23:02:48 -0400

    Lint

--- spec/features/send_feedback_spec.rb
+++ spec/features/send_feedback_spec.rb
@@ -11,6 +11,6 @@ RSpec.feature "Send feedback" do
     expect(page).to have_link("Send feedback")
 
     href = find_link("Send feedback")["href"]
-    expect(href).to match(/\/feedback$/)
+    expect(href).to match(%r{/feedback$})
   end
 end
