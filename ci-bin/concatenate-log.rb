0498b75611e5d0b5ba660075a993e7fb3c6039d9
Nemo Djuric <nemo.djuric@gmail.com>
2019-09-03 16:47:52 -0400

    Add efolder repo instead of caseflow

--- ci-bin/concatenate-log.rb
+++ ci-bin/concatenate-log.rb
@@ -8,7 +8,7 @@ require "open-uri"
 
 artifacts_url = "https://circleci.com/api/v1.1/project"\
                 "/github/department-of-veterans-affairs"\
-                "/caseflow/#{ENV['CIRCLE_BUILD_NUM']}/artifacts"
+                "/caseflow-efolder/#{ENV['CIRCLE_BUILD_NUM']}/artifacts"
 artifacts = JSON.parse(URI.parse(artifacts_url).read)
 
 log_file_urls = artifacts.map do |artifact|
