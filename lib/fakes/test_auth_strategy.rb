c065c4df84c97e32506cf5b44a9b577ab6ffff5f
Peter Karman <peter@truss.works>
2019-12-12 14:21:21 -0600

    sniffybara fixes

--- lib/fakes/test_auth_strategy.rb
+++ lib/fakes/test_auth_strategy.rb
@@ -1,10 +1,29 @@
 require "omniauth/strategies/developer"
 require "omniauth/form"
 
+class EfolderAuthForm < OmniAuth::Form
+  def header(title, header_info)
+    @html << <<-HTML
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <title>#{title}</title>
+      #{css}
+      #{header_info}
+    </head>
+    <body>
+    <h1>#{title}</h1>
+    <form method='post' #{"action='#{options[:url]}' " if options[:url]}noValidate='noValidate'>
+    HTML
+    self
+  end
+end
+
 class OmniAuth::Strategies::TestAuthStrategy < OmniAuth::Strategies::Developer
   # custom form rendering
   def request_phase
-    form = OmniAuth::Form.new(title: "Test VA Saml", url: callback_path)
+    form = EfolderAuthForm.new(title: "Test VA Saml", url: callback_path)
     options.fields.each do |field|
       form.text_field field.to_s.capitalize.tr("_", " "), field.to_s
     end
