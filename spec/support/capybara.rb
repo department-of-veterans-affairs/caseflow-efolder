11749b4fcaaf264fa7662a3769276fe0c0a70c39
Peter Karman <peter@truss.works>
2019-12-13 15:25:28 -0600

    simplify downloads tmp dir

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -10,8 +10,8 @@ Webdrivers.logger.level = :DEBUG if ENV["DEBUG"]
 Sniffybara::Driver.run_configuration_file = File.expand_path("VA-axe-run-configuration.json", __dir__)
 
 tmp_directory = Rails.root.join("tmp")
-download_directory = Rails.root.join("tmp/downloads_#{ENV['TEST_SUBCATEGORY'] || 'all'}")
-cache_directory = Rails.root.join("tmp/browser_cache_#{ENV['TEST_SUBCATEGORY'] || 'all'}")
+download_directory = Rails.root.join("tmp/downloads_all")
+cache_directory = Rails.root.join("tmp/browser_cache_all")
 
 Dir.mkdir tmp_directory unless File.directory?(tmp_directory)
 Dir.mkdir download_directory unless File.directory?(download_directory)
