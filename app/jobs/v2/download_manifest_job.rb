18d65e4b7929161b91b5b4d0dc146cfd43be1215
Peter Karman <peter@truss.works>
2020-01-03 13:33:25 -0600

    more debugging

--- app/jobs/v2/download_manifest_job.rb
+++ app/jobs/v2/download_manifest_job.rb
@@ -2,11 +2,14 @@ class V2::DownloadManifestJob < ActiveJob::Base
   queue_as :high_priority
 
   def perform(manifest_source, user = nil)
-    puts "start V2::DownloadManifestJob"
+    puts "start V2::DownloadManifestJob for #{manifest_source.name}"
     return if manifest_source.current?
     RequestStore.store[:current_user] = user if user
 
+    puts "ManifestFetcher.new.process"
     documents = ManifestFetcher.new(manifest_source: manifest_source).process
+
+    puts "found documents #{documents.length}"
 
     V2::SaveFilesInS3Job.perform_later(manifest_source) if documents.present? && !ApplicationController.helpers.ui_user?
   rescue StandardError => e
