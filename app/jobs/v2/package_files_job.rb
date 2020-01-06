71767088b7565a0c4802b5a3901a606d74fd971e
Peter Karman <peter@truss.works>
2020-01-06 12:26:43 -0600

    zap debug log

--- app/jobs/v2/package_files_job.rb
+++ app/jobs/v2/package_files_job.rb
@@ -2,7 +2,6 @@ class V2::PackageFilesJob < ActiveJob::Base
   queue_as :med_priority
 
   def perform(manifest)
-    puts "start V2::PackageFilesJob"
     return if manifest.recently_downloaded_files?
 
     ZipfileCreator.new(manifest: manifest).process
--- app/jobs/v2/save_files_in_s3_job.rb
+++ app/jobs/v2/save_files_in_s3_job.rb
@@ -2,7 +2,6 @@ class V2::SaveFilesInS3Job < ActiveJob::Base
   queue_as :low_priority
 
   def perform(manifest_source)
-    puts "start V2::SaveFilesInS3Job for #{manifest_source.name}"
     manifest_source.records.each(&:fetch!)
   end
 
