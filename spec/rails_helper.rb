a92a90e441103478a47f935f670941f34d2c7c57
Peter Karman <pkarman@users.noreply.github.com>
2020-01-16 12:52:36 -0600

    Revert rails52 rollback (#1176)
    * Revert "rollback to deployed/20200113_144730 (#1170)"
    
    This reverts commit 8e318cdc8b0a314a321186ade90dbe2fde5afd12.
    
    * use older streaming pattern

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,19 +9,6 @@ require_relative "../app/exceptions/standard_error"
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # The TZ variable controls the timezone of the browser in capybara tests, so we always define it.
@@ -48,9 +35,6 @@ def ensure_stable
 end
 
 RSpec.configure do |config|
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = false
 
   config.before(:all) do
@@ -61,23 +45,7 @@ RSpec.configure do |config|
     Rails.cache.clear
   end
 
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
 end
