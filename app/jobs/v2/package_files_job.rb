a25a268ad641829addc890401583f2d5ee2dca8f
Peter Karman <pkarman@users.noreply.github.com>
2020-02-26 12:31:14 -0600

    Add relevant Raven metadata to jobs (#1198)

--- app/jobs/v2/package_files_job.rb
+++ app/jobs/v2/package_files_job.rb
@@ -1,8 +1,10 @@
-class V2::PackageFilesJob < ActiveJob::Base
+class V2::PackageFilesJob < ApplicationJob
   queue_as :med_priority
 
   def perform(manifest)
     return if manifest.recently_downloaded_files?
+
+    Raven.extra_context(manifest: manifest.id)
 
     ZipfileCreator.new(manifest: manifest).process
   # Catch StandardError in case there is an error to avoid files downloads being stuck in pending state
