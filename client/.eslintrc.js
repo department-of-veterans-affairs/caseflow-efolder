716a29f25e294703daaaf48ea6e65dce19413896
JC Quirin <jcq@jcq.lan>
2020-03-25 15:42:54 -0700

    bumped ESLint; updated version of eslint-config-appeals;

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,7 +2,12 @@ module.exports = {
   env: {
     browser: true
   },
-  extends: [
-    '@department-of-veterans-affairs/eslint-config-appeals'
-  ]
+  extends: ['@department-of-veterans-affairs/eslint-config-appeals'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 10,
+    sourceType: 'module'
+  }
 };
