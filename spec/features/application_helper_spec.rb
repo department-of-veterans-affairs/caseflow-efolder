cf11703f51837b82a3e55a9eb1b7522a13259d46
Peter Karman <peter@truss.works>
2020-05-12 10:24:50 -0500

    yank more v1

--- spec/features/application_helper_spec.rb
+++ spec/features/application_helper_spec.rb
@@ -7,28 +7,4 @@ RSpec.describe ApplicationHelper, type: :helper do
       expect(helper.current_ga_path).to eq "/application/serve_single_page_app"
     end
   end
-
-  describe "#loading_pill" do
-    describe "by default" do
-      it "creates a pill labeled \"Loading...\"" do
-        expect(helper.loading_pill).to include "Loading..."
-      end
-
-      it "creates a spinning icon" do
-        expect(helper.loading_pill).to have_css(".ee-pill-icon-loading-front")
-      end
-    end
-
-    describe "with options[:text]" do
-      it "sets the pill's label" do
-        expect(helper.loading_pill(text: "Working...")).to include "Working..."
-      end
-    end
-
-    describe "with options[:no_icon]" do
-      it "omits the spinning icon" do
-        expect(helper.loading_pill(no_icon: true)).not_to have_css(".ee-pill-icon-loading-front")
-      end
-    end
-  end
 end
