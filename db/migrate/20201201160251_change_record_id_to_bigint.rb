af6d74e48f2e6902bc53b48f5265a27dcc288f05
Anya Roltsch <aroltsch@users.noreply.github.com>
2020-12-01 12:27:37 -0500

    Revert "Bigint for record ids (#1265)"
    This reverts commit 309fe48fa621eccc131f898b9ff6929883f85cc8.

--- db/migrate/20201201160251_change_record_id_to_bigint.rb
+++
@@ -1,9 +0,0 @@
-class ChangeRecordIdToBigint < ActiveRecord::Migration[5.2]
-  def up
-    change_column :records, :id, :bigint
-  end
-
-  def down
-    change_column :records, :id, :integer
-  end
-end
