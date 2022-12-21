f54255bc5c77251a2c8e83426e543484af76814b
mikefinneran <110622959+mikefinneran@users.noreply.github.com>
2022-12-21 16:38:37 -0500

    Revert "Revert "Mattray gov/appeals 10276 (#1334)" (#1339)"
    This reverts commit bc689ff670a7bf81cf35a34c3226c35c246bf561.

--- app/services/document_creator.rb
+++ app/services/document_creator.rb
@@ -6,6 +6,14 @@ class DocumentCreator
 
   def create
     ManifestSource.transaction do
+      ft_delta_documents = FeatureToggle.enabled?(:cache_delta_documents, user: current_user)
+    Rails.logger.info("Feature Toggle Cache Delta Documents Enabled? #{ft_delta_documents} for CSS ID: #{current_user&.css_id}")
+    if ft_delta_documents
+      external_documents.uniq(&:document_id).map do |document|
+        manifest_source.current? ? manifest_source.records.where(series_id: document.series_id).destroy_all : manifest_source.records.destroy_all
+        Record.create_from_external_document(manifest_source, document)
+      end
+    else
       # Re-create documents each time in order to use the latest versions
       # and if documents were deleted from VBMS
       manifest_source.records.delete_all
@@ -13,6 +21,8 @@ class DocumentCreator
       external_documents.uniq(&:document_id).map do |document|
         Record.create_from_external_document(manifest_source, document)
       end
+    end
+     
     end
   rescue ActiveRecord::RecordNotUnique
     # This can happen when two jobs are run simultaneously. Both jobs delete the records
@@ -28,5 +38,10 @@ class DocumentCreator
   # Override the getter to return only non-restricted documents
   def external_documents
     DocumentFilter.new(documents: @external_documents).filter
+  end
+  private
+
+  def current_user
+    RequestStore[:current_user]
   end
 end
