680653fc54065df6e2388e8b7fb519492ec46198
mikefinneran <110622959+mikefinneran@users.noreply.github.com>
2022-12-22 16:21:36 -0500

    improve spacing

--- app/services/document_creator.rb
+++ app/services/document_creator.rb
@@ -7,22 +7,21 @@ class DocumentCreator
   def create
     ManifestSource.transaction do
       ft_delta_documents = FeatureToggle.enabled?(:cache_delta_documents, user: current_user)
-    Rails.logger.info("Feature Toggle Cache Delta Documents Enabled? #{ft_delta_documents} for CSS ID: #{current_user&.css_id}")
-    if ft_delta_documents
-      external_documents.uniq(&:document_id).map do |document|
-        manifest_source.current? ? manifest_source.records.where(series_id: document.series_id).destroy_all : manifest_source.records.destroy_all
-        Record.create_from_external_document(manifest_source, document)
+      Rails.logger.info("Feature Toggle Cache Delta Documents Enabled? #{ft_delta_documents} for CSS ID: #{current_user&.css_id}")
+      if ft_delta_documents
+        external_documents.uniq(&:document_id).map do |document|
+          manifest_source.current? ? manifest_source.records.where(series_id: document.series_id).destroy_all : manifest_source.records.destroy_all
+          Record.create_from_external_document(manifest_source, document)
+        end
+      else
+        # Re-create documents each time in order to use the latest versions
+        # and if documents were deleted from VBMS
+        manifest_source.records.delete_all
+        # Remove any duplicates before adding them.
+        external_documents.uniq(&:document_id).map do |document|
+          Record.create_from_external_document(manifest_source, document)
+        end
       end
-    else
-      # Re-create documents each time in order to use the latest versions
-      # and if documents were deleted from VBMS
-      manifest_source.records.delete_all
-      # Remove any duplicates before adding them.
-      external_documents.uniq(&:document_id).map do |document|
-        Record.create_from_external_document(manifest_source, document)
-      end
-    end
-     
     end
   rescue ActiveRecord::RecordNotUnique
     # This can happen when two jobs are run simultaneously. Both jobs delete the records
@@ -39,6 +38,7 @@ class DocumentCreator
   def external_documents
     DocumentFilter.new(documents: @external_documents).filter
   end
+
   private
 
   def current_user
