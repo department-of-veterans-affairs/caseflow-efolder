8b5b55c2004ac9a6c3cae3dc25a055b54aaf505e
Matt Ray <matthew.ray@va.gov>
2023-02-16 12:27:49 -0600

    Added tests for linearize and optimize methods

--- spec/services/pdf_service_spec.rb
+++ spec/services/pdf_service_spec.rb
@@ -27,4 +27,37 @@ describe PdfService do
       expect(IO.read(filename)).to eq("plain text")
     end
   end
+
+  context "#linearize" do
+    IO.binwrite("lib/pdfs/test1.pdf", IO.binread("lib/pdfs/0.pdf"))
+    testPdf1 = "lib/pdfs/test1.pdf"
+    it "Linearizes pdf using qpdf" do
+      expect(IO.binread(testPdf1).include? "Linearized").to be false
+      PdfService.linearize_pdf(testPdf1)
+      expect(IO.binread(testPdf1).include? "Linearized").to be true
+    end
+       after(:context) do
+         system("rm #{testPdf1}")
+       end
+  end
+
+   context "#optimize_pdf" do
+    
+      IO.binwrite("lib/pdfs/test2.pdf", IO.binread("lib/pdfs/0.pdf"))
+      testpdf2 = "lib/pdfs/test2.pdf"
+      testpdf2_size_before = File.size("lib/pdfs/test2.pdf")
+
+     it "optimizes the pdf" do
+      expect(File.exists?(testpdf2)).to be true
+      PdfService.optimize(testpdf2)
+      expect(File.exists?(testpdf2)).to be false
+      expect(File.exists?("#{testpdf2}-optimized.pdf")).to be true
+      expect(File.size("#{testpdf2}-optimized.pdf")) .to be < testpdf2_size_before
+     end
+
+      after(:context) do
+       system("rm #{testpdf2}-optimized.pdf")
+     end
+   end
+  
 end
