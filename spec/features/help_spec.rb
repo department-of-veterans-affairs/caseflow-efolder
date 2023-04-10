4b96ae4766f7bb03d19a26e49943013bd5978a91
Enrilo Ugalde <ugalde_enrilo@bah.com>
2023-04-10 12:24:31 -0700

    removed un-need commented code

--- spec/features/help_spec.rb
+++ spec/features/help_spec.rb
@@ -9,9 +9,6 @@ RSpec.feature "Help" do
       # rubocop:disable Lint/HandleExceptions
     begin
       click_on "Help"
-      # rescue Capybara::Poltergeist::JavascriptError
-      # Embedding YouTube Links can cause JavaScript Errors on browsers that don't
-      # support HTML5 video. Since it's unrelated to our code, ignore this JS error.
     end
     # rubocop:enable Lint/HandleExceptions
     expect(page).to have_content("Frequently Asked Questions")
