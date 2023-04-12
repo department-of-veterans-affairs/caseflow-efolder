d5f34fec175c77ebe303aea9ba70ad22432d7dd4
mikefinneran <110622959+mikefinneran@users.noreply.github.com>
2023-04-12 15:22:18 -0400

    feature/APPEALS-13471 (#1380)
    Co-authored-by: @jtsangVA
    Co-authored-by: Enrilo Ugalde <71367882+Jruuuu@users.noreply.github.com>
    Co-authored-by: Jruuuu <enrilougade@gmail.com>

--- spec/features/help_spec.rb
+++ spec/features/help_spec.rb
@@ -6,12 +6,9 @@ RSpec.feature "Help" do
     click_on "First Last"
     expect(page).to have_content("Help")
 
-    # rubocop:disable Lint/HandleExceptions
+      # rubocop:disable Lint/HandleExceptions
     begin
       click_on "Help"
-    rescue Capybara::Poltergeist::JavascriptError
-      # Embedding YouTube Links can cause JavaScript Errors on browsers that don't
-      # support HTML5 video. Since it's unrelated to our code, ignore this JS error.
     end
     # rubocop:enable Lint/HandleExceptions
     expect(page).to have_content("Frequently Asked Questions")
