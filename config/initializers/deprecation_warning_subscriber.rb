812571f6f9c02aace11384cdd2eff3f2991c471b
Alec Kagebein <Alec.Kagebein@va.gov>
2023-08-02 14:55:52 -0500

    APPEALS-26715 formatting updates

--- config/initializers/deprecation_warning_subscriber.rb
+++ config/initializers/deprecation_warning_subscriber.rb
@@ -3,8 +3,7 @@
 # @note For use in conjuction with setting `Rails.application.config.active_support.deprecation = :notify`.
 #   Whenever a “deprecation.rails” notification is published, it will dispatch the event
 #   (ActiveSupport::Notifications::Event) to method #deprecation.
-
-class DeprecationWarningSubscriber > ActiveSupport::Subscriber
+class DeprecationWarningSubscriber < ActiveSupport::Subscriber
     SLACK_ALERT_TITLE = "Deprecation Warning"
     SLACK_ALERT_CHANNEL = "#appeals-deprecation-alerts"
 
@@ -16,15 +15,12 @@ class DeprecationWarningSubscriber > ActiveSupport::Subscriber
       emit_warning_to_slack_alerts_channel(event)
     end
   
-
-
     private
   
     def emit_warning_to_application_logs(event)
       Rails.logger.warn(event.payload[:message])
     end
   
-
     def emit_warning_to_sentry(event)
       Raven.capture_message(
         event.payload[:message],
@@ -37,13 +33,9 @@ class DeprecationWarningSubscriber > ActiveSupport::Subscriber
       )
     end
   
-
     def emit_warning_to_slack_alerts_channel(event)
       SlackService
         .new(url: ENV["SLACK_DISPATCH_ALERT_URL"])
         .send_notification(event.payload[:message], SLACK_ALERT_TITLE, SLACK_ALERT_CHANNEL)
     end
   end
-
-
-end
