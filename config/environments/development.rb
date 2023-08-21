8892655f980831474408a2e842e2de6aef72e98b
Alec Kagebein <Alec.Kagebein@va.gov>
2023-08-21 13:04:24 -0500

    APPEALS-26730 app:update development.rb changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,16 +16,16 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
-
   else
     config.action_controller.perform_caching = false
 
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
+  # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
@@ -50,28 +50,28 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-
+  
 #=========================================================================================
 # eFolder - Custom Config Settings
 # Keep all efolder specific config settings below for clean diff's when upgrading rails
 #=========================================================================================
-  config.vva_wsdl = "https://vbaphid521ldb.vba.va.gov:7002/VABFI/services/vva?wsdl"
+config.vva_wsdl = "https://vbaphid521ldb.vba.va.gov:7002/VABFI/services/vva?wsdl"
 
-  config.s3_enabled = true
-  config.s3_bucket_name = "dsva-appeals-efolder-demo"
+config.s3_enabled = true
+config.s3_bucket_name = "dsva-appeals-efolder-demo"
 
-  config.api_key = "token"
+config.api_key = "token"
 
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+# Raises error for missing translations
+# config.action_view.raise_on_missing_translations = true
 
-  config.sqs_create_queues = true
-  config.sqs_endpoint = 'http://localhost:4566'
+config.sqs_create_queues = true
+config.sqs_endpoint = 'http://localhost:4566'
 end
