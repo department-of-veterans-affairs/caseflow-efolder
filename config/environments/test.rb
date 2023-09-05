6dfc16a2191fdc341f1156da2b9d891605f9c822
AnandEdara <131183324+AnandEdara@users.noreply.github.com>
2023-09-05 04:38:36 -0500

    Update test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :raise
+  config.active_support.deprecation = :stderr
 
   config.s3_enabled = false
 
