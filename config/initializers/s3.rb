982911d892cfdf6a336f4dcd53328d91f4de72c6
Alec Kagebein <Alec.Kagebein@va.gov>
2023-09-20 12:03:54 -0500

    APPEALS-28929 added comments for DW

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,3 +1,7 @@
+# Wrapper required for depreciation warning fix as of 6.1
+# See the link below for more details
+# https://bill.harding.blog/2021/07/22/rails-6-1-deprecation-warning-initialization-autoloaded-the-constants-what-to-do-about-it/
+
 Rails.application.reloader.to_prepare do
   S3Service = (Rails.application.config.s3_enabled ? Caseflow::S3Service : Caseflow::Fakes::S3Service)
 end
--- config/initializers/vva.rb
+++ config/initializers/vva.rb
@@ -1,3 +1,7 @@
+# Wrapper required for depreciation warning fix as of 6.1
+# See the link below for more details
+# https://bill.harding.blog/2021/07/22/rails-6-1-deprecation-warning-initialization-autoloaded-the-constants-what-to-do-about-it/
+
 Rails.application.reloader.to_prepare do
   VVAService = (!BaseController.dependencies_faked? ? ExternalApi::VVAService : Fakes::VVAService)
 end
