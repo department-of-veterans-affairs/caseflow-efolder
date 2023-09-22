c09bfab56dbea78410c6e76429b4ef1b5f91077a
Alec Kagebein <Alec.Kagebein@va.gov>
2023-09-22 16:16:58 -0500

    APPEALS-28929 app:update config/initializers/filter_parameter_logging

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+]
+
+# What we had with 6.0
+#Rails.application.config.filter_parameters += [:password]
