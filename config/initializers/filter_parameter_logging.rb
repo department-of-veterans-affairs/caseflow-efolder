7a1c91d26c04211668cccc70330b669255ad1580
AKeyframe <89286339+AKeyframe@users.noreply.github.com>
2023-10-02 11:21:12 -0500

    comment removal
    Co-authored-by: Jeremy Croteau <jcroteau@users.noreply.github.com>

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,6 +4,3 @@
 Rails.application.config.filter_parameters += [
   :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
 ]
-
-# What we had with 6.0
-#Rails.application.config.filter_parameters += [:password]
