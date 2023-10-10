3a48d402001739e24a555de5dc035dfbab8776ed
Alec Kagebein <Alec.Kagebein@va.gov>
2023-10-10 11:38:21 -0500

    APPEALS-26838 activated zeitwerk autoloading

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module CaseflowEfolder
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.autoloader = :classic
+    # config.autoloader = :classic
 
     # Configuration for the application, engines, and railties goes here.
     #
