dd75a14b99b23fa94e82024728a950c811a5b5dc
Jeremy Croteau <jcroteau@users.noreply.github.com>
2023-10-12 19:30:56 -0400

    Alec k/appeals 26726 (#1452) (#1453)
    * APPEALS-26726 Rails updated to 5.2.8.1
    
    * APPEALS-26726 app:update bin/bundle changes
    
    * APPEALS-26726 app:update bin/rails changes
    
    * APPEALS-26726 app:update bin/rake changes
    
    * APPEALS-26726 app:update bin/setup changes
    
    * APPEALS-26726 app:update bin/update changes
    
    * APPEALS-26726 app:update bin/yarn changes
    
    * APPEALS-26726 app:update config/boot.rb changes
    
    * APPEALS-26726 bin/update recomment system(bin/yarn)
    
    * APPEALS-26726 app:update config/cable.yml changes
    
    * APPEALS-26726 app:update config/storage.yml changes
    
    * APPEALS-26726 app:update config/initializers/content_security_policy.rb changes
    
    * APPEALS-26726 app:update config/locales/en.yml changes
    
    * APPEALS-26726 app:update config/initializers/assets.rb changes
    
    * APPEALS-26726 app:update config/puma.rb changes
    
    * APPEALS-26726 app:update config/environments/development.rb changes
    
    * APPEALS-26726 app:update config/environments/production.rb changes
    
    * APPEALS-26726 app:update config/environments/test.rb changes
    
    * APPEALS-26726 app:update config/application.rb changes
    
    * APPEALS-26726 cleaned up and updated new_framework_defaulsts for 5.0-5.2
    
    * APPEALS-26726 pr corrections
    
    * APPEALS-26726 new_framework_defaults_5_2 removal
    
    * ⬇️ Downgrade Bundler back to 1.17.3
    
    The version of Bundler installed with RubyGems during the build process
    was not forwards compatible with 2.4.17, so lets change this back to 1.17.3
    for now
    
    ---------
    
    Co-authored-by: Alec Kagebein <Alec.Kagebein@va.gov>

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -7,16 +8,79 @@ Bundler.require(*Rails.groups)
 
 module CaseflowEfolder
   class Application < Rails::Application
-
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 5.1
-    
+    config.load_defaults 5.2
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
+    #=======================================================================================
+    # Rails 5.0 default overrides
+    #---------------------------------------------------------------------------------------
+    
+    # Enable per-form CSRF tokens. Previous versions had false.
+    # Deafault as of 5.0: true
+    Rails.application.config.action_controller.per_form_csrf_tokens = false
+
+    # Enable origin-checking CSRF mitigation. Previous versions had false.
+    # Default as of 5.0: true
+    Rails.application.config.action_controller.forgery_protection_origin_check = false
+
+    # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
+    # Previous versions had false.
+    # Default as of 5.0: true
+    ActiveSupport.to_time_preserves_timezone = false
+
+    # Require `belongs_to` associations by default. Previous versions had false.
+    # Default as of 5.0: true
+    Rails.application.config.active_record.belongs_to_required_by_default = false   
+
+
+    #=======================================================================================
+    # Rails 5.1 default overrides
+    #---------------------------------------------------------------------------------------
+    
+    # Make `form_with` generate non-remote forms.
+    # Default as of 5.1: true
+    Rails.application.config.action_view.form_with_generates_remote_forms = false
+
+
+    #=======================================================================================
+    # Rails 5.2 default overrides
+    #---------------------------------------------------------------------------------------
+
+    # Use AES-256-GCM authenticated encryption for encrypted cookies.
+    # Also, embed cookie expiry in signed or encrypted cookies for increased security.
+    #
+    # This option is not backwards compatible with earlier Rails versions.
+    # It's best enabled when your entire app is migrated and stable on 5.2.
+    #
+    # Existing cookies will be converted on read then written with the new scheme.
+    # Default as of 5.2: true
+    Rails.application.config.action_dispatch.use_authenticated_cookie_encryption = false
+
+    # Use AES-256-GCM authenticated encryption as default cipher for encrypting messages
+    # instead of AES-256-CBC, when use_authenticated_message_encryption is set to true.
+    # Default as of 5.2: true
+    Rails.application.config.active_support.use_authenticated_message_encryption = false
+
+    # Add default protection from forgery to ActionController::Base instead of in
+    # ApplicationController.
+    # Default as of 5.2: true
+    Rails.application.config.action_controller.default_protect_from_forgery = false
+
+    # Store boolean values are in sqlite3 databases as 1 and 0 instead of 't' and
+    # 'f' after migrating old data.
+    # Default as of 5.2: true
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = false
+
+
+    #=======================================================================================
+    # eFolder Specific configs
+    #---------------------------------------------------------------------------------------
     config.download_filepath = Rails.root + "tmp/files"
-
     config.autoload_paths += Dir[Rails.root + 'app/jobs']
     config.autoload_paths << Rails.root.join('lib')
     config.autoload_paths << Rails.root.join('lib/scripts')
