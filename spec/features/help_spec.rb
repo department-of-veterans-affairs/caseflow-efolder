d03ea73c8d3ce893ed74724316daee19c0b45aa8
Matthew Thornton <ThorntonMatthew@users.noreply.github.com>
2023-12-20 21:53:37 -0500

    Set pipefail env attribute and add intentional test failure

--- spec/features/help_spec.rb
+++ spec/features/help_spec.rb
@@ -11,6 +11,6 @@ RSpec.feature "Help" do
       click_on "Help"
     end
     # rubocop:enable Lint/HandleExceptions
-    expect(page).to have_content("Frequently Asked Questions")
+    expect(page).to have_content("Frequently Asked Answers")
   end
 end
