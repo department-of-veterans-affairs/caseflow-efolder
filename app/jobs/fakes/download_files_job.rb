3415cd9acf2c8eab48b90ab5e36bb6c1bf60522e
kshiflett88 <kodishiflett@gmail.com>
2024-01-08 15:29:04 -0500

    Caseflow to efolder naming

--- app/jobs/fakes/download_files_job.rb
+++ app/jobs/fakes/download_files_job.rb
@@ -2,10 +2,10 @@ class Fakes::DownloadFilesJob < ActiveJob::Base
   queue_as :med_priority
 
   def perform(download)
-    demo = Efolder::Fakes::DocumentService::DEMOS[download.file_number] || Casflow::Fakes::DocumentService::DEMOS["DEMODEFAULT"]
+    demo = Efolder::Fakes::DocumentService::DEMOS[download.file_number] || Efolder::Fakes::DocumentService::DEMOS["DEMODEFAULT"]
 
-    Casflow::Fakes::DocumentService.errors = demo[:error]
-    Casflow::Fakes::DocumentService.max_time = demo[:max_file_load]
+    Efolder::Fakes::DocumentService.errors = demo[:error]
+    Efolder::Fakes::DocumentService.max_time = demo[:max_file_load]
 
     download_documents = DownloadDocuments.new(download: download)
     download_documents.download_and_package
