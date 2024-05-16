24b851eabc2ee718f74aa4bd22d656c5f11c3cdf
SanthiParakal133 <132940479+SanthiParakal133@users.noreply.github.com>
2024-05-16 11:01:52 -0400

    Deepak/appeals 45276 (#1563)
    * update fetch_delta_documents_for with CE-api
    
    * update specs for fetch_delta_documents_for
    
    * update specs
    
    ---------
    
    Co-authored-by: youfoundmanesh <129548081+youfoundmanesh@users.noreply.github.com>

--- spec/services/external_api/vbms_service_spec.rb
+++ spec/services/external_api/vbms_service_spec.rb
@@ -24,4 +24,31 @@ describe ExternalApi::VBMSService do
       end
     end
   end
+
+  describe ".fetch_delta_documents_for" do
+    let(:mock_json_adapter) { instance_double(JsonApiResponseAdapter) }
+
+    before do
+      allow(JsonApiResponseAdapter).to receive(:new).and_return(mock_json_adapter)
+    end
+
+    context "with use_ce_api feature toggle enabled" do
+      before { FeatureToggle.enable!(:use_ce_api) }
+      after { FeatureToggle.disable!(:use_ce_api) }
+
+      it "calls the CE API" do
+        veteran_file_number = "123456789"
+        begin_date_range = "2024-05-10"
+        end_date_range = "2024-05-10"
+        expect(VeteranFileFetcher).to receive(:fetch_veteran_file_list_by_date_range)
+        .with(
+          veteran_file_number: veteran_file_number,
+          begin_date_range: begin_date_range,
+          end_date_range: end_date_range
+        )
+        expect(mock_json_adapter).to receive(:adapt_v2_fetch_documents_for).and_return([])
+        described.fetch_delta_documents_for(veteran_file_number, begin_date_range, end_date_range)
+      end
+    end
+  end
 end
