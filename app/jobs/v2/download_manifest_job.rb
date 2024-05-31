75a8acfd70e9c7a0ba9bbabf92c4a1bf2207e7b3
Alex Smith <alexander.smith6@va.gov>
2024-05-31 09:12:18 -0600

    Update download_manifest_job.rb
    Remove comment added during testing.

--- app/jobs/v2/download_manifest_job.rb
+++ app/jobs/v2/download_manifest_job.rb
@@ -11,7 +11,7 @@ class V2::DownloadManifestJob < ApplicationJob
 
     log_info(manifest_source.manifest.file_number, documents, manifest_source.manifest.zipfile_size)
 
-    #V2::SaveFilesInS3Job.perform_later(manifest_source) if documents.present? && !ApplicationController.helpers.ui_user?
+    V2::SaveFilesInS3Job.perform_later(manifest_source) if documents.present? && !ApplicationController.helpers.ui_user?
   rescue StandardError => error
     manifest_source.update!(status: :failed)
     Rails.logger.error "DownloadManifestJob encountered error #{error.class.name} when fetching manifest for appeal #{manifest_source.manifest.file_number}"
