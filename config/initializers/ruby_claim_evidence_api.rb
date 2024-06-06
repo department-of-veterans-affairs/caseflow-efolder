514f6c9dd38ab24c982fdc87f7ee806145638da8
Alex Smith <alexander.smith6@va.gov>
2024-06-06 11:02:10 -0600

    Add Logger to ruby_claim_evidence_api Initializer (#1598)
    * Add Logger to ruby_claim_evidence_api Initializer
    
    * Update to Latest ruby_claim_evidence_api Gem Version

--- config/initializers/ruby_claim_evidence_api.rb
+++ config/initializers/ruby_claim_evidence_api.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-VeteranFileFetcher = ExternalApi::VeteranFileFetcher.new(use_canned_api_responses: BaseController.dependencies_faked?)
+VeteranFileFetcher = ExternalApi::VeteranFileFetcher
+  .new(use_canned_api_responses: BaseController.dependencies_faked?, logger: Rails.logger)
