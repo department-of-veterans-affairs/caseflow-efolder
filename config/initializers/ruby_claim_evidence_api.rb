90bcc9b43c9e6e565a785a70ca95d4bdb8b63d48
sbashamoni <138054633+sbashamoni@users.noreply.github.com>
2024-07-11 13:35:33 -0400

    Revert "Release R1.6.0 FY24Q3.6 (#1626)"
    This reverts commit d2280537463193af4b698303ae7b9dabd62c3beb.

--- config/initializers/ruby_claim_evidence_api.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-VeteranFileFetcher = ExternalApi::VeteranFileFetcher
-  .new(use_canned_api_responses: BaseController.dependencies_faked_for_CEAPI?, logger: Rails.logger)
