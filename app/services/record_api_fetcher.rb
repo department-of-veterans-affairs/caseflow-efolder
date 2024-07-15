694af7e15e1719be7c86e83cb8a08d264adc4a02
Andre Pollard <andre.pollard@va.gov>
2024-07-15 16:12:26 -0400

    update tests for vbms to reader on s3 miss enabled

--- app/services/record_api_fetcher.rb
+++ app/services/record_api_fetcher.rb
@@ -1,24 +1,13 @@
-class RecordFetcher < RecordFetcherBase
+class RecordApiFetcher < RecordFetcherBase
 
   EXCEPTIONS = [VBMS::ClientError, VVA::ClientError].freeze
   SECONDS_TO_AUTO_UNLOCK = 90
 
   def process
-    s = Redis::Semaphore.new("record_#{record.id}".to_s,
-                             url: Rails.application.secrets.redis_url_cache,
-                             stale_client_timeout: 5,
-                             expiration: SECONDS_TO_AUTO_UNLOCK)
-    MetricsService.record("RecordFetcher Semaphore lock from VA manifest source name: #{record.manifest_source.name} for file_number #{record.file_number}",
-                          service: record.manifest_source.name.downcase.to_sym,
-                          name: "recordfetcher_semaphore_lock") do
-      s.lock(SECONDS_TO_AUTO_UNLOCK)
-    end
     content_from_s3 || content_from_va_service
   rescue *EXCEPTIONS => error
     Rails.logger.error("Caught #{error}")
     nil
-  ensure
-    s&.unlock
   end
 
   private
@@ -30,8 +19,9 @@ class RecordFetcher < RecordFetcherBase
                                     name: "v2_fetch_document_file") do
       record.service.v2_fetch_document_file(record)
     end
+
     content = image_converter(content)
-    content_store_to_s3(content)
+
     content
   end
 
