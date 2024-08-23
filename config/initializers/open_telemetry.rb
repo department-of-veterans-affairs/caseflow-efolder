dd92843c6882fa6b3873888293e661a922deddf9
raymond-hughes <raymond.hughes2@va.gov>
2024-08-23 12:15:22 -0400

    Matching eFolder Express to Caseflow for OTEL config and gems

--- config/initializers/open_telemetry.rb
+++ config/initializers/open_telemetry.rb
@@ -1,27 +1,25 @@
 # frozen_string_literal: true
+require 'rubygems'
+require 'bundler/setup'
 
-require "rubygems"
-require "bundler/setup"
+require 'opentelemetry/sdk'
+require 'opentelemetry/exporter/otlp'
 
 require "opentelemetry-instrumentation-action_pack"
 require "opentelemetry-instrumentation-action_view"
-# require "opentelemetry-instrumentation-active_job"
+require "opentelemetry-instrumentation-active_job"
 require "opentelemetry-instrumentation-active_record"
-# require "opentelemetry-instrumentation-active_model_serializers"
-# require "opentelemetry-instrumentation-active_support"
-# require "opentelemetry-instrumentation-aws_sdk"
-# require "opentelemetry-instrumentation-concurrent_ruby"
-# require "opentelemetry-instrumentation-faraday"
-# require "opentelemetry-instrumentation-http_client"
-# require "opentelemetry-instrumentation-net_http"
-# require "opentelemetry-instrumentation-pg"
+require "opentelemetry-instrumentation-active_support"
+require "opentelemetry-instrumentation-aws_sdk"
+require "opentelemetry-instrumentation-concurrent_ruby"
+require "opentelemetry-instrumentation-faraday"
+require "opentelemetry-instrumentation-http_client"
+require "opentelemetry-instrumentation-net_http"
+require "opentelemetry-instrumentation-pg"
 require "opentelemetry-instrumentation-rack"
 require "opentelemetry-instrumentation-rails"
-# require "opentelemetry-instrumentation-rake"
-# require "opentelemetry-instrumentation-redis"
-
-require "opentelemetry-exporter-otlp"
-require "opentelemetry-sdk"
+require "opentelemetry-instrumentation-rake"
+require "opentelemetry-instrumentation-redis"
 
 # rubocop:disable Layout/LineLength
 
@@ -32,18 +30,31 @@ Rails.logger.info("DT_API_TOKEN is set to #{DT_API_TOKEN}")
 
 if !Rails.env.development? && !Rails.env.test? && !Rails.env.demo?
   OpenTelemetry::SDK.configure do |c|
-    c.service_name = "efolder-quickstart"
-    c.service_version = "1.0.1"
-    c.use "OpenTelemetry::Instrumentation::Rails"
-    c.use "OpenTelemetry::Instrumentation::Rack", untraced_endpoints: ["/health-check", "/sample", "/logs"]
-    c.use "OpenTelemetry::Instrumentation::ActiveRecord"
-    c.use "OpenTelemetry::Instrumentation::ActionView"
-    c.use "OpenTelemetry::Instrumentation::ActionPack"
+    c.service_name = 'ruby-quickstart'
+    c.service_version = '1.0.1'
+
+    c.use 'OpenTelemetry::Instrumentation::ActiveRecord'
+    c.use 'OpenTelemetry::Instrumentation::Rack', { untraced_endpoints: ['/health-check', '/sample', '/logs'] }
+    c.use 'OpenTelemetry::Instrumentation::Rails'
+
+    # c.use 'OpenTelemetry::Instrumentation::ActionPack'
+    # c.use 'OpenTelemetry::Instrumentation::ActionView'
+    # c.use 'OpenTelemetry::Instrumentation::ActiveJob'
+    # c.use 'OpenTelemetry::Instrumentation::ActiveSupport'
+    # c.use 'OpenTelemetry::Instrumentation::AwsSdk'
+    # c.use 'OpenTelemetry::Instrumentation::ConcurrentRuby'
+    # c.use 'OpenTelemetry::Instrumentation::Faraday'
+    # c.use 'OpenTelemetry::Instrumentation::HttpClient'
+    # c.use 'OpenTelemetry::Instrumentation::Redis'
+    # c.use 'OpenTelemetry::Instrumentation::Net::HTTP'
+    # c.use 'OpenTelemetry::Instrumentation::PG'
+
+    Rails.logger.info("Loaded instruments")
 
     %w[dt_metadata_e617c525669e072eebe3d0f08212e8f2.properties /var/lib/dynatrace/enrichment/dt_host_metadata.properties].each { |name|
       begin
         c.resource = OpenTelemetry::SDK::Resources::Resource.create(Hash[*File.read(name.start_with?("/var") ? name : File.read(name)).split(/[=\n]+/)])
-      rescue # rubocop:disable Lint/SuppressedException
+      rescue
       end
     }
     c.add_span_processor(
@@ -58,13 +69,4 @@ if !Rails.env.development? && !Rails.env.test? && !Rails.env.demo?
     )
   end
 end
-# Uncomment this line to enbale console exporting.
-# ENV["OTEL_TRACES_EXPORTER"] = "console"
-if Rails.env.development? && ENV["OTEL_TRACES_EXPORTER"] == "console"
-  OpenTelemetry::SDK.configure do |c|
-    c.service_name = "efolder-quickstart"
-    c.service_version = "1.0.1"
-    c.use_all(config)
-  end
-end
-# rubocop:enable Layout/LineLength
+  # rubocop:enable Layout/LineLength
