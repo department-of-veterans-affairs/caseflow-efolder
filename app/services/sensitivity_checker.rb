7c242c915193694bb39be718d712c315d5ea3133
Alex Smith <alexander.smith6@va.gov>
2024-09-04 09:32:36 -0600

    Fix Missing Manifest User Causing Sensitivity Check Failures (#1675)
    * Fix User Missing for BGS Sensitivity Check
    
    - Update the manifests_controller's refresh method to use the
      find_or_create_by_user method to find a manifest. This will
      ensure that the user is set correctly for BGS calls.
    
    - Move SensitivityLevelCheckFailure logic to the base_controller.
    
    * Fix Misc. Issues
    
    - Move rescue_from for BGS errors into the API V1 controller so
      its existing standard error rescue doesn't catch this exception.
    
    - Improve manifests_controller request spec with sensitivity check
      logic.

--- app/services/sensitivity_checker.rb
+++ app/services/sensitivity_checker.rb
@@ -4,8 +4,8 @@ class SensitivityChecker
   def sensitivity_levels_compatible?(user:, veteran_file_number:)
     bgs_service.sensitivity_level_for_user(user) >=
       bgs_service.sensitivity_level_for_veteran(veteran_file_number)
-  rescue StandardError => error
-    ExceptionLogger.capture(error)
+  rescue StandardError => e
+    ExceptionLogger.capture(e)
 
     false
   end
