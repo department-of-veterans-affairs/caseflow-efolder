b8698ed3a826a3138c8611e19b9394afa3424b4f
Alexander Smith <alex.smith.99@thunderyard.com>
2024-09-13 10:24:10 -0600

    Add current_user to SaveFilesInS3 Job
    - Since the SaveFilesInS3 job is spawned by another job, it does
      not have access to RequestStore[:current_user] which is needed
      for verifying veteran/user sensitivity compatibility.
    
    - This PR also fixes several Rubocop violations in various files.

--- app/jobs/v2/save_files_in_s3_job.rb
+++ app/jobs/v2/save_files_in_s3_job.rb
@@ -1,8 +1,14 @@
 class V2::SaveFilesInS3Job < ApplicationJob
   queue_as :low_priority
 
-  def perform(manifest_source)
-    Raven.extra_context(manifest_source: manifest_source.id)
+  def perform(manifest_source, user_id)
+    Raven.extra_context(manifest_source: manifest_source.id, user_id: user_id)
+
+    # Set user for permission check if the user is blank
+    if FeatureToggle.enabled?(:check_user_sensitivity) && RequestStore[:current_user].blank?
+      user = User.find(user_id)
+      RequestStore.store[:current_user] = user
+    end
 
     manifest_source.records.each(&:fetch!)
   end
