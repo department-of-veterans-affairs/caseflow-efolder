9ffdb8ba3f0f7d190f5b8f6bc3e5ce599df30263
Alec Kagebein <Alec.Kagebein@va.gov>
2024-09-15 11:51:36 -0500

    Refactored bgs_errors.rb and bgs.rb

--- config/initializers/bgs.rb
+++ config/initializers/bgs.rb
@@ -1,3 +1,8 @@
-Rails.application.reloader.to_prepare do
-  BGSService = (!BaseController.dependencies_faked? ? ExternalApi::BGSService : Fakes::BGSService)
+module BGS
+  class InvalidUsername < StandardError; end
+  class InvalidStation < StandardError; end
+  class InvalidApplication < StandardError; end
+  class NoActiveStations < StandardError; end
+  class NoCaseflowAccess < StandardError; end
+  class StationAssertionRequired < StandardError; end
 end
