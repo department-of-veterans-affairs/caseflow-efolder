6c3b1ad77c9c5706f656e15046ecbb2ff189ccbe
Alec Kagebein <Alec.Kagebein@va.gov>
2024-09-15 04:46:14 -0500

    Added to_prepare's - Solves DW on autoloading initialized constants

--- config/initializers/vbms.rb
+++ config/initializers/vbms.rb
@@ -1,1 +1,3 @@
-VBMSService = (!BaseController.dependencies_faked? ? ExternalApi::VBMSService : Fakes::VBMSService)
+Rails.application.reloader.to_prepare do
+  VBMSService = (!BaseController.dependencies_faked? ? ExternalApi::VBMSService : Fakes::VBMSService)
+end
