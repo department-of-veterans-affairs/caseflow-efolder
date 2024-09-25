c13cb12a345ee7b201215d50245aa22bb90ae70f
youfoundmanesh <maneshreddy.kommidi@va.gov>
2024-09-25 15:11:26 -0400

    Update feature toogle to send_current_user_cred

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -31,7 +31,7 @@ class Manifest < ApplicationRecord
     # Reset stale manifests.
     update!(fetched_files_status: :initialized) if ready_for_refresh?
 
-    if FeatureToggle.enabled?(:check_user_sensitivity)
+    if FeatureToggle.enabled?(:send_current_user_cred)
       if sensitivity_checker.sensitivity_levels_compatible?(
         user: user,
         veteran_file_number: file_number
