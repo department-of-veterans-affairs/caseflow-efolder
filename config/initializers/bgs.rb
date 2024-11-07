2e59a15c359a6fc01ada718eb389101569a564ab
AKeyframe <89286339+AKeyframe@users.noreply.github.com>
2024-11-07 09:52:16 -0600

    Feature/APPEALS-44512-Zeitwerk-auto-transition (#1738)
    * Update Caseflow-Commons for Zeitwerk
    
    * Remove classic loading, added Zeitwerk loading options
    
    * Added to_prepare's - Solves DW on autoloading initialized constants
    
    * Added Zeitwerk initializer file
    
    * Refactored bgs_errors.rb and bgs.rb
    
    * Remove require statements
    
    * Added Fakes and TestAuthStrategy modules
    
    * Readded accidental require deletion
    
    * Removed plural from CurrentUserLoadTests
    
    * Refactored for One File One Constant
    
    * Added Zeitwerk Rspec test
    
    * Fix Rubocop setup - removed incorrect include setup
    
    * Added custom Rubocop - Top Level Constatns Per File
    
    * Rename and moved Zeitwerk spec for Caseflow parity
    
    * Fixed custom cop spec file structure
    
    * Zeitwerk spec comments addressed
    
    * Refactored for Caseflow parity
    
    * Renamed for consistency
    
    * Safety require
    
    * Safety require and to_prepare for BGS
    
    * Added autoload/eagerload section
    
    * Renamed for consistency
    
    * Readded require so to_prepare could be removed
    
    * Added rubocop_todo for temp ignore linter errors
    
    * Required changes for .rubocop.yml to work
    
    * Updated Rubocop todo with proper generation
    
    * Unecessary ShellCommand removed
    
    * Ignored TestAuthStrategy in autoloading - unneeded
    
    * Update .rubocop.yml
    
    Co-authored-by: Jeremy Croteau <jcroteau@users.noreply.github.com>
    
    * Update app/services/external_api/vbms_service.rb
    
    Co-authored-by: Jeremy Croteau <jcroteau@users.noreply.github.com>
    
    * Add #{root} to config/application.rb
    
    Co-authored-by: Jeremy Croteau <jcroteau@users.noreply.github.com>
    
    * Cosmetic Update config/initializers/shoryuken.rb
    
    Co-authored-by: Jeremy Croteau <jcroteau@users.noreply.github.com>
    
    * Cosmetic Update config/initializers/shoryuken.rb
    
    Co-authored-by: Jeremy Croteau <jcroteau@users.noreply.github.com>
    
    * Cosmetic Update config/initializers/shoryuken.rb
    
    Co-authored-by: Jeremy Croteau <jcroteau@users.noreply.github.com>
    
    * Cosmetic Update lib/fakes/test_auth_strategy.rb
    
    Co-authored-by: Jeremy Croteau <jcroteau@users.noreply.github.com>
    
    * Added after_initialize to prevent multiple subscribers on relead
    
    * Added before_initialize so Error class is loaded before anything else
    
    * ✏️ Fix typo in filename
    
    * 🚚 Move spec file to proper location
    
    * ♻️ Lexically order filters in .simplecov
    
    * 🔧 Filter `lib/efolder/migration.rb` from simplecov coverage
    
    * ⬆️ Update `caseflow-commons` dependecy
    
    ---------
    
    Co-authored-by: Jeremy Croteau <jcroteau@users.noreply.github.com>
    Co-authored-by: Jeremy Croteau <jeremiah.croteau@va.gov>

--- config/initializers/bgs.rb
+++ config/initializers/bgs.rb
@@ -1,1 +1,13 @@
-BGSService = (!BaseController.dependencies_faked? ? ExternalApi::BGSService : Fakes::BGSService)
+Rails.application.config.to_prepare do
+  require "bgs"
+  
+  module BGS
+    class InvalidUsername < StandardError; end
+    class InvalidStation < StandardError; end
+    class InvalidApplication < StandardError; end
+    class NoActiveStations < StandardError; end
+    class NoCaseflowAccess < StandardError; end
+    class StationAssertionRequired < StandardError; end
+  end
+end
+
